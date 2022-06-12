ded77aad0137518d48700e326037393e61553ef0
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-06-12 18:00:52 -0400

    mtypes fixes for modern platforms.

--- src/mtypes.h
+++ src/mtypes.h
@@ -2,7 +2,14 @@
 #ifndef MTYPES_H
 #define MTYPES_H
 
-#ifdef PLATFORM_PALM
+/**
+ * stdint.h is great, but it's C99 and thus not available on older platforms.
+ *
+ * This header attempts to provide a specific but cross-platform
+ * implementation that can be called from legacy projects.
+ */
+
+#if defined( PLATFORM_PALM )
 
 typedef UInt8 uint8_t;
 typedef Int8 int8_t;
@@ -11,7 +18,7 @@ typedef Int16 int16_t;
 typedef UInt32 uint32_t;
 typedef Int32 int32_t;
 
-#else
+#elif defined( PLATFORM_WIN )
 
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
@@ -20,7 +27,11 @@ typedef unsigned short uint16_t;
 typedef long int32_t;
 typedef unsigned long uint32_t;
 
-#endif /* PLATFORM_PALM */
+#else
+
+#include <stdint.h>
+
+#endif /* PLATFORM_PALM || PLATFORM_WIN */
 
 #endif /* !MTYPES_H */
 
