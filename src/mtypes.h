54d0fb8964eecf00ceec7f5c3fa0347569d6047b
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-06-12 17:52:26 -0400

    Added mtypes.

---
+++ src/mtypes.h
@@ -0,0 +1,26 @@
+
+#ifndef MTYPES_H
+#define MTYPES_H
+
+#ifdef PLATFORM_PALM
+
+typedef UInt8 uint8_t;
+typedef Int8 int8_t;
+typedef UInt16 uint16_t;
+typedef Int16 int16_t;
+typedef UInt32 uint32_t;
+typedef Int32 int32_t;
+
+#else
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef long int32_t;
+typedef unsigned long uint32_t;
+
+#endif /* PLATFORM_PALM */
+
+#endif /* !MTYPES_H */
+
