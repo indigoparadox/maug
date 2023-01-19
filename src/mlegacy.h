d31f925033954dc140bdcfddc8d102741c43d6e2
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-01-18 21:32:37 -0500

    Straightened out debug log level on release.

--- src/mlegacy.h
+++ src/mlegacy.h
@@ -2,6 +2,10 @@
 #ifndef MLEGACY_H
 #define MLEGACY_H
 
+#ifdef ANCIENT_C
+#define  UPRINTF_ANCIENT_C
+#endif /* ANCIENT_C */
+
 #if defined( PLATFORM_PALM )
 #  define MAUG_OS_PALM
 #elif defined( PLATFORM_WIN ) || defined( RETROFLAT_OS_WIN )
