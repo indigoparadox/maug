c2cd28d0c05afc29345ab3808d96af39e0bea9cc
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-03-25 20:00:15 -0400

    Locking and allocation for CSS.

--- src/mcss.h
+++ src/mcss.h
@@ -2,6 +2,21 @@
 #ifndef MCSS_H
 #define MCSS_H
 
+#ifndef MCSS_PARSER_STYLES_INIT_SZ
+#  define MCSS_PARSER_STYLES_INIT_SZ 10
+#endif /* !MCSS_PARSER_STYLES_INIT_SZ */
+
+#define mcss_parser_lock( parser ) \
+   if( NULL == (parser)->styles ) { \
+      maug_mlock( (parser)->styles_h, (parser)->styles ); \
+      maug_cleanup_if_null_alloc( struct MCSS_STYLE*, (parser)->styles ); \
+   }
+
+#define mcss_parser_unlock( parser ) \
+   if( NULL != (parser)->styles ) { \
+      maug_munlock( (parser)->styles_h, (parser)->styles ); \
+   }
+
 #define MCSS_PROP_TABLE( f ) \
    f( 0, WIDTH, size_t ) \
    f( 1, HEIGHT, size_t ) \
@@ -26,6 +41,34 @@ struct MCSS_PARSER {
    size_t styles_sz;
    size_t styles_sz_max;
 };
+
+#ifdef MCSS_C
+
+MERROR_RETVAL mcss_init_parser( struct MCSS_PARSER* parser ) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   assert( 0 == parser->styles_sz );
+   assert( (MAUG_MHANDLE)NULL == parser->styles );
+
+   /* Perform initial tag allocation. */
+   parser->styles_sz_max = MCSS_PARSER_STYLES_INIT_SZ;
+   debug_printf( 1, "allocating " SIZE_T_FMT " styles...",
+      parser->styles_sz_max );
+   parser->styles_h = maug_malloc(
+      parser->styles_sz_max, sizeof( struct MCSS_STYLE ) );
+   assert( (MAUG_MHANDLE)NULL != parser->styles_h );
+   if( NULL == parser->styles_h ) {
+      error_printf( "unable to allocate " SIZE_T_FMT " styles!",
+         parser->styles_sz_max );
+      goto cleanup;
+   }
+
+cleanup:
+
+   return retval;
+}
+
+#endif /* MCSS_C */
 
 #endif /* !MCSS_H */
 
