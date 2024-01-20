b7877e02cb548935f2466e9fc9b5485d8d680d14
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-01-20 00:34:48 -0500

    Rough initial draft of retrofil.h for reading binary files.

---
+++ src/retrofil.h
@@ -0,0 +1,108 @@
+
+#ifndef RETROFIL_H
+#define RETROFIL_H
+
+MERROR_RETVAL retrofil_open_read(
+   const char* filename, MAUG_MHANDLE* p_bytes_ptr, size_t* p_bytes_sz );
+
+void retrofil_close_read( MAUG_MHANDLE bytes_ptr_h, size_t bytes_sz );
+
+#ifdef RETROFIL_C
+
+#ifdef RETROFLAT_OS_UNIX
+#  include <sys/mman.h> /* mmap() */
+#  include <unistd.h> /* close() */
+#  include <fcntl.h> /* open() */
+#  include <sys/stat.h> /* fstat() */
+#else
+#  include <stdio.h>
+#endif /* RETROFLAT_OS_UNIX */
+
+MERROR_RETVAL retrofil_open_read(
+   const char* filename, MAUG_MHANDLE* p_bytes_ptr_h, size_t* p_bytes_sz
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+#ifdef RETROFLAT_OS_UNIX
+   struct stat st;
+   int in_file = 0;
+#else
+   size_t bytes_rd = 0;
+   FILE* in_file = NULL;
+   uint8_t* bytes_ptr = NULL;
+#endif /* !RETROFLAT_OS_UNIX */
+
+#ifdef RETROFLAT_OS_UNIX
+
+   in_file = open( filename, O_RDONLY );
+   if( 0 >= in_file ) {
+      error_printf( "could not open file: %s", filename );
+      retval = MERROR_FILE;
+      goto cleanup;
+   }
+
+   fstat( in_file, &st );
+
+   *p_bytes_ptr_h = 
+      mmap( (caddr_t)0, st.st_size, PROT_READ, MAP_SHARED,  in_file, 0 );
+   maug_cleanup_if_null_alloc( uint8_t*, *p_bytes_ptr_h );
+   *p_bytes_sz = st.st_size;
+
+cleanup:
+
+   if( 0 < in_file ) {
+      close( in_file );
+   }
+
+#else
+
+   in_file = fopen( filename, "rb" );
+   if( NULL == in_file ) {
+      error_printf( "could not open file: %s", filename );
+      retval = MERROR_FILE;
+      goto cleanup;
+   }
+
+   fseek( in_file, 0, SEEK_END );
+   *p_bytes_sz = ftell( in_file );
+   fseek( in_file, 0, SEEK_SET );
+
+   *p_bytes_ptr_h = maug_malloc( 1, *p_bytes_sz );
+   maug_cleanup_if_null_alloc( MAUG_MHANDLE, *p_bytes_ptr_h );
+
+   maug_mlock( *p_bytes_ptr_h, bytes_ptr );
+   maug_cleanup_if_null_alloc( uint8_t*, bytes_ptr );
+
+   bytes_rd = fread( bytes_ptr, 1, *p_bytes_sz, in_file );
+   if( bytes_rd < *p_bytes_sz ) {
+      error_printf( "could not read entire file!" );
+      retval = MERROR_FILE;
+      goto cleanup;
+   }
+
+cleanup:
+
+   if( NULL != bytes_ptr ) {
+      maug_munlock( *p_bytes_ptr_h, bytes_ptr );
+   }
+
+   if( NULL != in_file ) {
+      fclose( in_file );
+   }
+
+#endif /* RETROFLAT_OS_UNIX */
+
+   return retval;
+}
+
+void retrofil_close_read( MAUG_MHANDLE bytes_ptr_h, size_t bytes_sz ) {
+#ifdef RETROFLAT_OS_UNIX
+   munmap( bytes_ptr_h, bytes_sz );
+#else
+   maug_mfree( bytes_ptr_h );
+#endif /* RETROFLAT_OS_UNIX */
+}
+
+#endif /* RETROFIL_C */
+
+#endif /* !RETROFIL_H */
+
