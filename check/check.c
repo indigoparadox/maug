6a2cb65639a5e36c4115c1ebba708dc8dfd86adc
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-02-02 22:16:03 -0500

    Roughly starting RLE decode, added checks to help.

---
+++ check/check.c
@@ -0,0 +1,27 @@
+
+#define MAUG_C
+#include <maug.h>
+
+#include <check.h>
+
+#define main_add_test_proto( suite_name ) \
+   Suite* suite_name ## _suite();
+
+#define main_add_test( suite_name ) \
+   Suite* s_ ## suite_name = suite_name ## _suite(); \
+   SRunner* sr_ ## suite_name = srunner_create( s_ ## suite_name ); \
+   /* srunner_set_fork_status( sr_ ## suite_name, CK_NOFORK ); */ \
+   srunner_run_all( sr_ ## suite_name, CK_VERBOSE ); \
+   number_failed += srunner_ntests_failed( sr_ ## suite_name ); \
+   srunner_free( sr_ ## suite_name );
+
+main_add_test_proto( mfmt )
+
+int main( void ) {
+   int number_failed = 0;
+
+   main_add_test( mfmt );
+
+   return( number_failed == 0 ) ? 0 : 1;
+}
+
