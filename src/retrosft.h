8e596414a9cc14b2500011952053b1e1ee20339b
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-02-11 19:17:54 -0500

    Bounds checking for soft lines.

--- src/retrosft.h
+++ src/retrosft.h
@@ -241,8 +241,15 @@ void retrosoft_line(
       end[for_axis] > iter[for_axis] ;
       iter[for_axis]++
    ) {
-      retroflat_px(
-         target, color, iter[RETROFLAT_LINE_X], iter[RETROFLAT_LINE_Y], 0 );
+
+      if(
+         (size_t)iter[RETROFLAT_LINE_X] < retroflat_screen_w() &&
+         (size_t)iter[RETROFLAT_LINE_Y] < retroflat_screen_h()
+      ) {
+         retroflat_px(
+            target, color,
+            iter[RETROFLAT_LINE_X], iter[RETROFLAT_LINE_Y], 0 );
+      }
 
       /* Increment off-axis based on for-axis. */
       if( 0 < delta ) {
