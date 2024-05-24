55c5d3c4e145905371581c3b076713bde4d371d3
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-05-23 21:05:31 -0400

    Added DOS stub for retrofnt.

---
+++ dosstubs/retrofnt.c
@@ -0,0 +1,9 @@
+
+#ifndef RETROFLAT_NO_RETROGUI
+#  define MAUG_NO_RETRO
+#  include <maug.h>
+#  include <retroflt.h>
+#  define RETROFNT_C
+#  include <retrofnt.h>
+#endif /* !RETROFLAT_NO_RETROGUI */
+
