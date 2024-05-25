916a8128e69f2e9dbfd2c1a299c913f92a2e8c00
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-05-25 02:09:43 -0400

    DOS stub fixes.

--- dosstubs/mhtmr.c
+++ dosstubs/mhtmr.c
@@ -2,6 +2,10 @@
 #ifndef MAUG_NO_MHTML
 #  define MHTMR_C
 #  include <maug.h>
+#  include <retroflt.h>
+#  include <retrofnt.h>
+#  include <retrogxc.h>
+#  include <retrogui.h>
 #  include <mhtml.h>
 #  include <mhtmr.h>
 #endif /* !MAUG_NO_MHTML */
