de1b791a987429ae1217f3ab5e83809434cadf4a
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-05-29 21:59:28 -0400

    Add stub for retrocon.

---
+++ dosstubs/retrocon.c
@@ -0,0 +1,12 @@
+
+#ifndef RETROFLAT_NO_RETROCON
+#  define MAUG_NO_RETRO
+#  include <maug.h>
+#  include <retroflt.h>
+#  include <retrofnt.h>
+#  include <retrogxc.h>
+#  include <retrogui.h>
+#  define RETROCON_C
+#  include <retrocon.h>
+#endif /* !RETROFLAT_NO_RETROCON */
+
