e8b10e736a1a19a93bc827754f00c6de499faa25
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-07-04 22:43:34 -0400

    Started pulling out GLUT API.

--- api/glut/retapif.h
+++ api/glut/retapif.h
@@ -1,3 +1,140 @@
 
+#ifndef RETPLTF_H
+#define RETPLTF_H
+
 /* TODO */
+void retroflat_message(
+   uint8_t flags, const char* title, const char* format, ...
+) {
+   char msg_out[RETROFLAT_MSG_MAX + 1];
+   va_list vargs;
+#  ifdef RETROFLAT_API_SDL2
+   uint32_t sdl_msg_flags = 0;
+#  elif (defined( RETROFLAT_API_SDL1 ) && defined( RETROFLAT_OS_WIN ))
+   uint32_t win_msg_flags = 0;
+#  endif
+
+   memset( msg_out, '\0', RETROFLAT_MSG_MAX + 1 );
+   va_start( vargs, format );
+   maug_vsnprintf( msg_out, RETROFLAT_MSG_MAX, format, vargs );
+
+#  if defined( RETROFLAT_OS_WIN )
+   switch( (flags & RETROFLAT_MSG_FLAG_TYPE_MASK) ) {
+   case RETROFLAT_MSG_FLAG_ERROR:
+      win_msg_flags |= MB_ICONSTOP;
+      break;
+
+   case RETROFLAT_MSG_FLAG_INFO:
+      win_msg_flags |= MB_ICONINFORMATION;
+      break;
+
+   case RETROFLAT_MSG_FLAG_WARNING:
+      win_msg_flags |= MB_ICONEXCLAMATION;
+      break;
+   }
+
+   MessageBox( retroflat_root_win(), msg_out, title, win_msg_flags );
+
+#  else  
+
+   /* TODO */
+   error_printf( "%s", msg_out );
+
+#  endif
+
+   va_end( vargs );
+}
+
+/* === */
+
+void retroflat_set_title( const char* format, ... ) {
+   char title[RETROFLAT_TITLE_MAX + 1];
+   va_list vargs;
+
+   /* Build the title. */
+   va_start( vargs, format );
+   memset( title, '\0', RETROFLAT_TITLE_MAX + 1 );
+   maug_vsnprintf( title, RETROFLAT_TITLE_MAX, format, vargs );
+
+   glutSetWindowTitle( title );
+
+   va_end( vargs );
+}
+
+/* === */
+
+retroflat_ms_t retroflat_get_ms() {
+   return glutGet( GLUT_ELAPSED_TIME );
+}
+
+/* === */
+
+uint32_t retroflat_get_rand() {
+   return rand();
+}
+
+/* === */
+
+int retroflat_draw_lock( struct RETROFLAT_BITMAP* bmp ) {
+   return retroglu_draw_lock( bmp );
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_draw_release( struct RETROFLAT_BITMAP* bmp ) {
+   return retroglu_draw_release( bmp );
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_load_bitmap(
+   const char* filename, struct RETROFLAT_BITMAP* bmp_out, uint8_t flags
+) {
+   char filename_path[RETROFLAT_PATH_MAX + 1];
+   MERROR_RETVAL retval = MERROR_OK;
+
+   assert( NULL != bmp_out );
+   maug_mzero( bmp_out, sizeof( struct RETROFLAT_BITMAP ) );
+   retval = retroflat_build_filename_path(
+      filename, filename_path, RETROFLAT_PATH_MAX + 1, flags );
+   maug_cleanup_if_not_ok();
+   debug_printf( 1, "retroflat: loading bitmap: %s", filename_path );
+
+   retval = retroglu_load_bitmap( filename_path, bmp_out, flags );
+
+cleanup:
+
+   return retval;
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_create_bitmap(
+   size_t w, size_t h, struct RETROFLAT_BITMAP* bmp_out, uint8_t flags
+) {
+   maug_mzero( bmp_out, sizeof( struct RETROFLAT_BITMAP ) );
+
+   bmp_out->sz = sizeof( struct RETROFLAT_BITMAP );
+
+   return retroglu_create_bitmap( w, h, bmp_out, flags );
+}
+
+/* === */
+
+void retroflat_destroy_bitmap( struct RETROFLAT_BITMAP* bmp ) {
+   retroglu_destroy_bitmap( bmp );
+}
+
+/* === */
+
+void retroflat_blit_bitmap(
+   struct RETROFLAT_BITMAP* target, struct RETROFLAT_BITMAP* src,
+   int s_x, int s_y, int d_x, int d_y, int16_t w, int16_t h
+) {
+   assert( NULL != src );
+
+   retroglu_blit_bitmap( target, src, s_x, s_y, d_x, d_y, w, h );
+}
+
+#endif /* !RETPLTF_H */
 
--- src/retroflt.h
+++ src/retroflt.h
@@ -946,130 +946,7 @@ typedef int RETROFLAT_COLOR_DEF;
 
 #elif defined( RETROFLAT_API_GLUT )
 
-#  ifndef RETROFLAT_OPENGL
-#     error "RETROFLAT_API_GLUT specified without RETROFLAT_OPENGL!"
-#     define RETROFLAT_OPENGL
-#  endif /* !RETROFLAT_OPENGL */
-
-#  ifdef __WATCOMC__
-#     define GLUT_DISABLE_ATEXIT_HACK
-#  endif
-
-#  include <GL/glut.h>
-
-#  ifndef RETROFLAT_CONFIG_USE_FILE
-#     define RETROFLAT_CONFIG_USE_FILE
-#  endif /* !RETROFLAT_CONFIG_USE_FILE */
-
-typedef int16_t RETROFLAT_IN_KEY;
-typedef uint32_t retroflat_ms_t;
-
-#  define RETROFLAT_MS_FMT "%lu"
-
-typedef float MAUG_CONST* RETROFLAT_COLOR_DEF;
-
-struct RETROFLAT_BITMAP {
-   size_t sz;
-   uint8_t flags;
-   struct RETROFLAT_GLTEX tex;
-   ssize_t w;
-   ssize_t h;
-};
-
-#  define retroflat_bitmap_ok( bitmap ) (NULL != (bitmap)->b)
-#  define retroflat_bitmap_locked( bmp ) \
-      (RETROFLAT_FLAGS_LOCK == (RETROFLAT_FLAGS_LOCK & (bmp)->flags))
-/* TODO */
-#  define retroflat_bitmap_w( bmp ) (0)
-#  define retroflat_bitmap_h( bmp ) (0)
-
-/*! \brief Special lock used before per-pixel operations because of SDL1. */
-#  define retroflat_px_lock( bmp )
-#  define retroflat_px_release( bmp )
-#  ifdef RETROFLAT_VDP
-#     define retroflat_vdp_lock( bmp )
-#     define retroflat_vdp_release( bmp )
-#  endif /* RETROFLAT_VDP */
-#  define retroflat_screen_w() (g_retroflat_state->screen_v_w)
-#  define retroflat_screen_h() (g_retroflat_state->screen_v_h)
-#  define retroflat_screen_buffer() (&(g_retroflat_state->buffer))
-#  define retroflat_root_win() (NULL) /* TODO */
-#  define retroflat_quit( retval_in ) glutDestroyWindow( glutGetWindow() )
-#  define END_OF_MAIN()
-
-#  define GLUT_SPECIAL_KEY_OFFSET 0x80
-
-#  define RETROFLAT_MOUSE_B_LEFT    -1
-#  define RETROFLAT_MOUSE_B_RIGHT   -2
-
-#  define RETROFLAT_KEY_UP	      (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_UP)
-#  define RETROFLAT_KEY_DOWN     (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_DOWN)
-#  define RETROFLAT_KEY_RIGHT	   (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_RIGHT)
-#  define RETROFLAT_KEY_LEFT	   (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_LEFT)
-#  define RETROFLAT_KEY_HOME	   (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_HOME)
-#  define RETROFLAT_KEY_END	   (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_END)
-#  define RETROFLAT_KEY_PGUP     (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_PAGE_UP)
-#  define RETROFLAT_KEY_PGDN     \
-   (GLUT_SPECIAL_KEY_OFFSET + GLUT_KEY_PAGE_DOWN)
-#  define RETROFLAT_KEY_DELETE   0x7f
-#  define RETROFLAT_KEY_ESC      0x1b
-#  define RETROFLAT_KEY_ENTER    0x0d
-#  define RETROFLAT_KEY_TAB	   '\t'
-#  define RETROFLAT_KEY_SPACE	   ' '
-#  define RETROFLAT_KEY_GRAVE    '`'
-#  define RETROFLAT_KEY_SLASH    '/'
-#  define RETROFLAT_KEY_BKSP     0x08
-#  define RETROFLAT_KEY_SEMICOLON   ';'
-#  define RETROFLAT_KEY_PERIOD   '.'
-#  define RETROFLAT_KEY_COMMA    ','
-#  define RETROFLAT_KEY_EQUALS   '='
-#  define RETROFLAT_KEY_DASH     '-'
-#  define RETROFLAT_KEY_A		   'a'
-#  define RETROFLAT_KEY_B		   'b'
-#  define RETROFLAT_KEY_C		   'c'
-#  define RETROFLAT_KEY_D		   'd'
-#  define RETROFLAT_KEY_E		   'e'
-#  define RETROFLAT_KEY_F		   'f'
-#  define RETROFLAT_KEY_G		   'g'
-#  define RETROFLAT_KEY_H		   'h'
-#  define RETROFLAT_KEY_I		   'i'
-#  define RETROFLAT_KEY_J		   'j'
-#  define RETROFLAT_KEY_K		   'k'
-#  define RETROFLAT_KEY_L		   'l'
-#  define RETROFLAT_KEY_M		   'm'
-#  define RETROFLAT_KEY_N		   'n'
-#  define RETROFLAT_KEY_O		   'o'
-#  define RETROFLAT_KEY_P		   'p'
-#  define RETROFLAT_KEY_Q		   'q'
-#  define RETROFLAT_KEY_R		   'r'
-#  define RETROFLAT_KEY_S		   's'
-#  define RETROFLAT_KEY_T		   't'
-#  define RETROFLAT_KEY_U		   'u'
-#  define RETROFLAT_KEY_V		   'v'
-#  define RETROFLAT_KEY_W		   'w'
-#  define RETROFLAT_KEY_X		   'x'
-#  define RETROFLAT_KEY_Y		   'y'
-#  define RETROFLAT_KEY_Z		   'z'
-#  define RETROFLAT_KEY_0		   '0'
-#  define RETROFLAT_KEY_1		   '1'
-#  define RETROFLAT_KEY_2		   '2'
-#  define RETROFLAT_KEY_3		   '3'
-#  define RETROFLAT_KEY_4		   '4'
-#  define RETROFLAT_KEY_5		   '5'
-#  define RETROFLAT_KEY_6		   '6'
-#  define RETROFLAT_KEY_7		   '7'
-#  define RETROFLAT_KEY_8		   '8'
-#  define RETROFLAT_KEY_9		   '9'
-/* TODO */
-#  define RETROFLAT_KEY_BRACKETL '['
-/* TODO */
-#  define RETROFLAT_KEY_BRACKETR ']'
-/* TODO */
-#  define RETROFLAT_KEY_BACKSLASH   '\\'
-/* TODO */
-#  define RETROFLAT_KEY_QUOTE    '\''
-/* TODO */
-#  define RETROFLAT_KEY_EQUALS    '='
+/* See retapid.h for GLUT. */
 
 #elif defined( RETROFLAT_API_PC_BIOS )
 
@@ -3831,7 +3708,7 @@ void retroflat_set_title( const char* format, ... ) {
 #elif defined( RETROFLAT_API_LIBNDS )
    /* Do nothing. */
 #elif defined( RETROFLAT_API_GLUT )
-   glutSetWindowTitle( title );
+
 #else
 #     pragma message( "warning: set title implemented" )
 #  endif /* RETROFLAT_API_ALLEGRO || RETROFLAT_API_SDL */
@@ -3855,10 +3732,6 @@ retroflat_ms_t retroflat_get_ms() {
    return ((TIMER1_DATA * (1 << 16)) + TIMER0_DATA) / 32;
 
 #  elif defined( RETROFLAT_API_GLUT )
-
-   /* == GLUT == */
-
-   return glutGet( GLUT_ELAPSED_TIME );
 
 #  elif defined( RETROFLAT_API_PC_BIOS )
 
