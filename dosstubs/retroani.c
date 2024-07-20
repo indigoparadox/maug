98cb04c176602b77ac925dccbb4945f63c5505c3
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-07-19 23:29:23 -0400

    Added DOS stub for retroani.

---
+++ dosstubs/retroani.c
@@ -0,0 +1,9 @@
+
+#ifndef RETROFLAT_NO_RETROCON
+#  define MAUG_NO_RETRO
+#  include <maug.h>
+#  include <retroflt.h>
+#  define RETROANI_C
+#  include <retroani.h>
+#endif /* !RETROFLAT_NO_RETROCON */
+
