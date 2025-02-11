91ddb3286a3c641cde7d8c1ade5624ca95da688f
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-02-10 19:08:28 -0500

    Work on fleshing out achievement structs.

--- src/retroacv.h
+++ src/retroacv.h
@@ -2,19 +2,50 @@
 #ifndef RETROACV_H
 #define RETROACV_H
 
-MERROR_RETVAL retroacv_init();
+#ifndef RETROACV_ACHIEVEMENT_ID_SZ_MAX
+#  define RETROACV_ACHIEVEMENT_ID_SZ_MAX 10
+#endif /* !RETROACV_ACHIEVEMENT_ID_SZ_MAX */
 
-void retroacv_shutdown();
+#define RETROACV_FLAG_ACHIEVED 0x01
+
+struct RETROACV_ACHIEVEMENT {
+   uint8_t flags;
+   char id[RETROACV_ACHIEVEMENT_ID_SZ_MAX];
+};
+
+struct RETROACV_LIST {
+   uint8_t flags;
+   struct RETROGUI* gui;
+   size_t acv_count;
+   struct RETROACV_ACHIEVEMENT* acv;
+}
+
+MERROR_RETVAL retroacv_init( struct RETROACV_LIST* acv_list );
+
+MERROR_RETVAL retroacv_update(
+   struct RETROACV_LIST* acv_list, char* id, uint8_t flags );
+
+MERROR_RETVAL retroacv_save( struct RETROACV_LIST* acv_list );
+
+void retroacv_shutdown( struct RETROACV_LIST* acv_list );
 
 #ifdef RETROACV_C
 
-MERROR_RETVAL retroacv_init() {
+MERROR_RETVAL retroacv_init( struct RETROACV_LIST* acv_list ) {
    MERROR_RETVAL retval = MERROR_OK;
+
+   if( NULL == acv_list ) {
+      retval = MERROR_ALLOC;
+      goto cleanup;
+   }
+
+
+cleanup:
 
    return retval;
 };
 
-void retroacv_shutdown() {
+void retroacv_shutdown( struct RETROACV_LIST* acv_list ) {
 
 };
 
