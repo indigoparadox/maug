bb25f588a5e4a8cde9d6ae9ef0afc2860a25d20e
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-02-19 17:57:06 -0500

    Comitting stragglers.

--- src/mgl.h
+++ src/mgl.h
@@ -10,16 +10,46 @@
 #  define MGL_TRI_STACK_SZ_MAX 128
 #endif /* !MGL_TRI_STACK_SZ_MAX */
 
+#ifndef MGL_TRI_ACTIVE_SZ_MAX
+#  define MGL_TRI_ACTIVE_SZ_MAX 16
+#endif /* !MGL_TRI_ACTIVE_SZ_MAX */
+
+#define MGL
+
+#define MGL_SCALE_MULT 0
+#define MGL_SCALE_BIT_RIGHT 1
+
 /**
  * \addtogroup maug_mgl_gl
  * \{
  */
 
-#define glVertex2d( x, y ) glVertex3d( x, y, 0 )
-
 #define GL_TRIANGLES 1
 
+#define glVertex2i( x, y ) glVertex3i( x, y, 0 )
+
+#define glScaler( x, y, z ) \
+   g_mgl_matrix.scale_type = MGL_SCALE_BIT_RIGHT; \
+   g_mgl_matrix.scale_x = x; \
+   g_mgl_matrix.scale_y = y; \
+   g_mgl_matrix.scale_z = z; \
+   debug_printf( 1, "matrix scale shift right: %d, %d, %d", \
+      g_mgl_matrix.scale_x, \
+      g_mgl_matrix.scale_y, \
+      g_mgl_matrix.scale_z );
+
 /*! \} */ /* maug_mgl_gl */
+
+#define mgl_vertex_set( vn, vo, vi ) \
+   switch( g_mgl_matrix.scale_type ) { \
+   case MGL_SCALE_MULT: \
+      vo = vi * g_mgl_matrix.vn; \
+      break; \
+   case MGL_SCALE_BIT_RIGHT: \
+      vo = vi >> g_mgl_matrix.vn; \
+      debug_printf( 1, "v %d shifted right %d: %d", vi, g_mgl_matrix.vn, vo ) \
+      break; \
+   }
 
 struct MGL_VERTEX {
    int16_t x;
@@ -31,6 +61,13 @@ struct MGL_TRIANGLE {
    int16_t v[3];
    RETROFLAT_COLOR color;
    int16_t sz;
+};
+
+struct MGL_MATRIX {
+   int scale_type;
+   int scale_x;
+   int scale_y;
+   int scale_z;
 };
 
 /**
@@ -52,7 +89,7 @@ void glBegin( int16_t gl_poly_type );
 
 void glEnd();
 
-void glVertex3d( int16_t x, int16_t y, int16_t z );
+void glVertex3i( int16_t x, int16_t y, int16_t z );
 
 void glFlush();
 
@@ -65,6 +102,9 @@ uint16_t g_mgl_vertex_stack_sz = 0;
 struct MGL_TRIANGLE g_mgl_tri_stack[MGL_TRI_STACK_SZ_MAX];
 uint16_t g_mgl_tri_stack_sz = 0;
 RETROFLAT_COLOR g_mgl_color = 0;
+uint16_t g_mgl_tri_active[MGL_TRI_ACTIVE_SZ_MAX];
+uint16_t g_mgl_tri_active_sz = 0;
+struct MGL_MATRIX g_mgl_matrix;
 
 int mglt_vertex_inside(
    int16_t x0, int16_t y0, int16_t x1, int16_t y1,
@@ -93,15 +133,19 @@ void glColorr( RETROFLAT_COLOR color ) {
    g_mgl_color = color;  
 }
 
-void glVertex3d( int16_t x, int16_t y, int16_t z ) {
+void glVertex3i( int16_t x, int16_t y, int16_t z ) {
 
    assert( MGL_VERTEX_STACK_SZ_MAX > g_mgl_vertex_stack_sz );
    assert( 0 < g_mgl_tri_stack_sz );
 
    /* Add the vertex to the system vertex stack. */
-   g_mgl_vertex_stack[g_mgl_vertex_stack_sz].x = x;
-   g_mgl_vertex_stack[g_mgl_vertex_stack_sz].y = y;
-   g_mgl_vertex_stack[g_mgl_vertex_stack_sz].z = z;
+   mgl_vertex_set( scale_x, g_mgl_vertex_stack[g_mgl_vertex_stack_sz].x, x );
+   mgl_vertex_set( scale_y, g_mgl_vertex_stack[g_mgl_vertex_stack_sz].y, y );
+   mgl_vertex_set( scale_z, g_mgl_vertex_stack[g_mgl_vertex_stack_sz].z, z );
+   debug_printf( 1, "%d %d %d",
+      g_mgl_vertex_stack[g_mgl_vertex_stack_sz].x,
+      g_mgl_vertex_stack[g_mgl_vertex_stack_sz].y,
+      g_mgl_vertex_stack[g_mgl_vertex_stack_sz].z );
 
    /* Add the vertex to the current triangle. */
    g_mgl_tri_stack[g_mgl_tri_stack_sz - 1].v[
@@ -155,11 +199,13 @@ void glFlush() {
 
    g_mgl_vertex_stack_sz = 0;
    g_mgl_tri_stack_sz = 0;
+   memset( &g_mgl_matrix, '\0', sizeof( struct MGL_MATRIX ) );
 }
 
 #else
 
 extern RETROFLAT_COLOR g_mgl_color;
+extern struct MGL_MATRIX g_mgl_matrix;
 
 #endif /* MGL_C */
 
