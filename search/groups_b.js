c4a30f66fdd71477891b32b2dbf29bb4b8b39029
indigoparadox <indigoparadox@users.noreply.github.com>
2025-03-12 00:38:10 +0000

    deploy: ad063d59193d9526105f78bc0239584b875d1c51

---
+++ search/groups_b.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['management_20api_0',['Memory Management API',['../group__maug__mem.html',1,'']]],
+  ['maug_20file_20format_20api_1',['Maug File Format API',['../group__maug__fmt.html',1,'']]],
+  ['maug_20file_20format_3a_20bitmap_2',['Maug File Format: Bitmap',['../group__maug__fmt__bmp.html',1,'']]],
+  ['maug_20retro_20fixed_20point_3',['Maug Retro Fixed Point',['../group__maug__retrofp.html',1,'']]],
+  ['memory_20management_20api_4',['Memory Management API',['../group__maug__mem.html',1,'']]],
+  ['message_20api_5',['RetroFlat Message API',['../group__maug__retroflt__msg__api.html',1,'']]],
+  ['micro_20printf_20api_6',['Micro-PrintF API',['../group__maug__uprintf.html',1,'']]],
+  ['mlisp_20abstract_20syntax_20tree_20parser_7',['MLISP Abstract Syntax Tree Parser',['../group__mlisp__parser.html',1,'']]],
+  ['mlisp_20execution_20stack_8',['MLISP Execution Stack',['../group__mlisp__stack.html',1,'']]],
+  ['mlisp_20interpreter_9',['MLISP Interpreter',['../group__mlisp.html',1,'']]],
+  ['mlisp_20types_10',['MLISP Types',['../group__mlisp__types.html',1,'']]]
+];
---
+++ search/typedefs_1.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['retroflat_5fasset_5fpath_0',['retroflat_asset_path',['../group__maug__retroflt__assets.html#gab661d4bb88b3a50ac5a3f729d3c0afe0',1,'retroflt.h']]],
+  ['retroflat_5fcolor_1',['RETROFLAT_COLOR',['../group__maug__retroflt__color.html#ga64674353770a68f722cb4d5fc90db90d',1,'retroflt.h']]],
+  ['retroflat_5floop_5fiter_2',['retroflat_loop_iter',['../group__maug__retroflt.html#ga7f16b39955b5f77e5535e2198e9e0faa',1,'retroflt.h']]],
+  ['retroflat_5fvdp_5fproc_5ft_3',['retroflat_vdp_proc_t',['../group__maug__retroflt__vdp.html#gac542ec7b64f30a2d19bdbbbc7ef0aaa3',1,'retroflt.h']]],
+  ['retroglu_5fmtl_5fcb_4',['retroglu_mtl_cb',['../group__maug__retroglu__obj__fsm.html#gac3fbadc0d5212c1d69830e5a387875cd',1,'retroglu.h']]],
+  ['retrogui_5fidc_5ft_5',['retrogui_idc_t',['../group__maug__retrogui.html#ga5cef009ca9ec165db3aa58486b74bf80',1,'retrogui.h']]]
+];
