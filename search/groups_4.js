b98122ddb6a0b0da6ea9d5c75f2c24ccae2e4795
indigoparadox <indigoparadox@users.noreply.github.com>
2025-04-19 21:55:08 +0000

    deploy: e7457cf62427856313403e17c205209e5e6daa69

---
+++ search/groups_4.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['data_20memory_20api_0',['Data Memory API',['../group__maug__data.html',1,'']]],
+  ['data_20memory_20string_20pools_1',['Data Memory String Pools',['../group__mdata__strpool.html',1,'']]],
+  ['data_20memory_20tables_2',['Data Memory Tables',['../group__mdata__table.html',1,'']]],
+  ['data_20memory_20vectors_3',['Data Memory Vectors',['../group__mdata__vector.html',1,'']]],
+  ['definitions_4',['Definitions',['../group__maug__retroflt__keydefs.html',1,'RetroFlat Key Definitions'],['../group__retrotile__defs.html',1,'RetroTile Tile Definitions']]],
+  ['direction_20api_5',['RetroFlat Direction API',['../group__maug__retroflt__dir.html',1,'']]],
+  ['drawing_20api_6',['RetroFlat Drawing API',['../group__maug__retroflt__drawing.html',1,'']]]
+];
---
+++ search/typedefs_1.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['retroflat_5fasset_5fpath_0',['retroflat_asset_path',['../group__maug__retroflt__assets.html#gab661d4bb88b3a50ac5a3f729d3c0afe0',1,'retroflt.h']]],
+  ['retroflat_5fcolor_1',['RETROFLAT_COLOR',['../group__maug__retroflt__color.html#ga64674353770a68f722cb4d5fc90db90d',1,'retroflt.h']]],
+  ['retroflat_5floop_5fiter_2',['retroflat_loop_iter',['../group__maug__retroflt.html#ga7f16b39955b5f77e5535e2198e9e0faa',1,'retroflt.h']]],
+  ['retroflat_5fpx_5fcb_3',['retroflat_px_cb',['../group__maug__retroflt__dir__2d.html#ga04916a2aabc9a6fbe91042b36628ac73',1,'retroflt.h']]],
+  ['retroflat_5fpxxy_5ft_4',['retroflat_pxxy_t',['../group__maug__retroflt__dir__2d.html#ga936904b2e8f6bf38656cf3ef39139b06',1,'retroflt.h']]],
+  ['retroflat_5ftile_5ft_5',['retroflat_tile_t',['../group__retrotile.html#gad364ef207b2999e486363ca5ad948811',1,'retroflt.h']]],
+  ['retroflat_5fvdp_5fproc_5ft_6',['retroflat_vdp_proc_t',['../group__maug__retroflt__vdp.html#gac542ec7b64f30a2d19bdbbbc7ef0aaa3',1,'retroflt.h']]],
+  ['retrogui_5fidc_5ft_7',['retrogui_idc_t',['../group__maug__retrogui.html#ga5cef009ca9ec165db3aa58486b74bf80',1,'retrogui.h']]]
+];
