90f5709cf4a27985542462f9a79e9b1590bbbfbe
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-06-21 20:13:50 -0400

    File path traversal works!

--- api/file/mac68k/mrapifil.h
+++ api/file/mac68k/mrapifil.h
@@ -105,59 +105,85 @@ MERROR_RETVAL mfile_plt_open_read( const char* filename, mfile_t* p_file ) {
    char pas_dir_name[MAUG_PATH_MAX + 2];
    CInfoPBRec pb;
    OSErr pb_err;
-   int32_t last_dir_id = 0;
+   int32_t last_dir_id = 0,
+      iter_id = 0;
    Str255 vol_name;
 
    /* Get current working directory. */
    GetVol( vol_name, &vol_ref );
-   GetWDInfo( vol_ref, &vol_ref, &last_dir_id, nil );
+   GetWDInfo( vol_ref, &vol_ref, &iter_id, nil );
+   debug_printf( 1, "vol: %s", &(vol_name[1]) );
 
+   /* Iterate subdirectories from the current working directory by breaking
+    * the path up by '/' separators.
+    */
    while( MERROR_OK == tok_retval ) {
       maug_mzero( dir_name, MAUG_PATH_MAX + 1 );
       maug_mzero( pas_dir_name, MAUG_PATH_MAX + 2 );
 
+      /* Get the current path token. */
       tok_retval = maug_tok_str(
          dir_idx, filename, strlen( filename ), dir_name, MAUG_PATH_MAX, "/" );
 
       if( MERROR_OK == tok_retval ) {
          debug_printf( 1, "dir: %s", dir_name );
 
+         /* Translate the path token into a pascal string. */
          retval = maug_str_c2p( dir_name, pas_dir_name, MAUG_PATH_MAX + 1 );
          maug_cleanup_if_not_ok_msg( "filename string too long!" );
 
-         /* Find the subdirectory. */
+         /* Get info for item represented by path token. */
          maug_mzero( &pb, sizeof( CInfoPBRec ) );
          pb.hFileInfo.ioNamePtr = (StringPtr)pas_dir_name;
          pb.hFileInfo.ioVRefNum = vol_ref;
          pb.hFileInfo.ioFDirIndex = 0; /* Search by name. */
-         pb.hFileInfo.ioDirID = last_dir_id;
+         pb.hFileInfo.ioDirID = iter_id;
          pb_err = PBGetCatInfoSync( &pb );
 
          if( noErr != pb_err ) {
             /* Whatever this is, it's not a directory. */
-            error_printf( "unable to open directory: %s", dir_name );
-            break;
+            error_printf( "unable to open path item: %s", dir_name );
+            retval = MERROR_FILE;
+            goto cleanup;
          }
 
+         /* Stow the last directory for later and keep going. */
+         last_dir_id = iter_id;
+         iter_id = pb.hFileInfo.ioDirID;
+
          debug_printf(
-            1, "opened dir: %s (" S32_FMT ")", dir_name, last_dir_id );
+            1, "opened item: %s (id: " S32_FMT ")", dir_name, iter_id );
+
+         /* Figure out if this is a subdirectory or a file that can't be
+          * traversed any further.
+          */
+         if( !(pb.hFileInfo.ioFlAttrib & ioDirMask) ) {
+            debug_printf( 1, "item is a file!" );
+            break;
+         }
 
          dir_idx++;
       }
    }
 
-   /* Open file from the directory. */
-   pb_err = FSOpen(
-      (StringPtr)pas_dir_name, last_dir_id, &(p_file->h.file_ref) );
+   /* Open file with name from the last *directory* found in the path above. */
+   pb_err = HOpen(
+      vol_ref, last_dir_id, (StringPtr)pas_dir_name,
+      fsRdPerm, &(p_file->h.file_ref) );
    if( noErr != pb_err ) {
       error_printf(
-         "unable to open file \"%s\" (%u): %d",
-         &(pas_dir_name[1]), *((uint8_t*)pas_dir_name), pb_err );
+         "unable to open file \"%s\" (len: %u, dir_id: " S32_FMT "): %d",
+         &(pas_dir_name[1]), *((uint8_t*)pas_dir_name), last_dir_id, pb_err );
       retval = MERROR_FILE;
       goto cleanup;
    }
 
-   debug_printf( 1, "opened file %s (" OFF_T_FMT " bytes)...",
+   /* pb was setup by the traversal above, which should've hit this file last
+    * (even if it aborted once it determined it wasn't a directory)!
+    */
+   p_file->sz = pb.hFileInfo.ioFlLgLen;
+
+   debug_printf( 1, "opened file %s (sz: " OFF_T_FMT " bytes)...",
       filename, p_file->sz );
 
    p_file->type = MFILE_CADDY_TYPE_FILE;
