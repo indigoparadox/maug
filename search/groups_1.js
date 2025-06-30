b8a46a9b816541e20736a2829eee8bc77db4f54b
indigoparadox <indigoparadox@users.noreply.github.com>
2025-06-30 03:49:42 +0000

    deploy: 15d15acd1a523bfbb919ad96efb2f514fe312538

---
+++ search/groups_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['bitmap_0',['Maug File Format: Bitmap',['../group__maug__fmt__bmp.html',1,'']]],
+  ['bitmap_20api_1',['RetroFlat Bitmap API',['../group__maug__retroflt__bitmap.html',1,'']]],
+  ['byte_20order_2',['RetroFile Byte Order',['../group__maug__mfile__byte__order.html',1,'']]]
+];
---
+++ search/groups_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['order_0',['RetroFile Byte Order',['../group__maug__mfile__byte__order.html',1,'']]],
+  ['overlay_20api_1',['RetroGLU Overlay API',['../group__maug__retroglu__overlay.html',1,'']]]
+];
