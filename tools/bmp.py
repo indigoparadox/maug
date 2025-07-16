19a1a8b994fe3efded6041d28b0e80a46cf95b72
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-07-16 00:57:42 -0400

    Break bitmap reading into class.

--- tools/bmp.py
+++ tools/bmp.py
@@ -3,6 +3,55 @@
 import argparse
 import logging
 
+class Bitmap( object ):
+
+    def __init__( self, in_bmp_f ):
+
+        logger = logging.getLogger( 'bitmap.init' )
+
+        # Seek past signature.
+        in_bmp_f.seek( 2 )
+
+        # Grab file size.
+        self.bmp_sz = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
+        logger.debug( 'bitmap size: %d bytes', self.bmp_sz )
+
+        # Seek past reserved.
+        in_bmp_f.seek( 10 )
+
+        # Grab pixel offset.
+        self.px_offset = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
+        logger.debug( 'bitmap pixel offset: %d bytes', self.px_offset )
+
+        # Grab header size.
+        self.header_sz = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
+        logger.debug( 'bitmap header size: %d bytes', self.header_sz )
+
+        # Grab image width.
+        self.width = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
+        logger.debug( 'bitmap width: %d bytes', self.width )
+
+        # Grab image height.
+        self.height = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
+        logger.debug( 'bitmap height: %d bytes', self.height )
+
+        self._f = in_bmp_f
+
+    def pixels( self, draw_px_cb, pad_ln_cb ):
+
+        for y in range( 0, self.height ):
+            # Seek to the next line (the horizontal lines are backwards, so
+            # start at the end of the file and processing horizontal lines
+            # in reverse).
+            self._f.seek(
+                self.px_offset + (((self.height - 1) - y) * self.width) )
+
+            # Process this row.
+            for x in range( 0, self.width ):
+                px = int.from_bytes( self._f.read( 1 ) )
+                draw_px_cb( px )
+            pad_ln_cb( y )
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -22,46 +71,12 @@ def main():
 
     # Open the bitmap and parse it.
     with open( args.in_bmp, 'rb' ) as in_bmp_f:
-        # Seek past signature.
-        in_bmp_f.seek( 2 )
 
-        # Grab file size.
-        in_bmp_sz = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
-        logger.debug( 'bitmap size: %d bytes', in_bmp_sz )
-
-        # Seek past reserved.
-        in_bmp_f.seek( 10 )
-
-        # Grab pixel offset.
-        in_bmp_offs = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
-        logger.debug( 'bitmap pixel offset: %d bytes', in_bmp_offs )
-
-        # Grab header size.
-        in_bmp_hdr_sz = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
-        logger.debug( 'bitmap header size: %d bytes', in_bmp_hdr_sz )
-
-        # Grab image width.
-        in_bmp_w = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
-        logger.debug( 'bitmap width: %d bytes', in_bmp_w )
-
-        # Grab image height.
-        in_bmp_h = int.from_bytes( in_bmp_f.read( 4 ), 'little' )
-        logger.debug( 'bitmap height: %d bytes', in_bmp_h )
-
-        for y in range( 0, 16 ):
-            # Seek to the next line (the horizontal lines are backwards, so
-            # start at the end of the file and processing horizontal lines
-            # in reverse).
-            in_bmp_f.seek( in_bmp_offs + (((in_bmp_h - 1) - y) * in_bmp_w) )
-
-            # Process this row.
-            for x in range( 0, in_bmp_w ):
-                px = int.from_bytes( in_bmp_f.read( 1 ) )
-                if 0 < px:
-                    print( f"{hex( px )} ", end='' )
-                else:
-                    print( '    ', end='' )
-            print( '' )
+        bmp = Bitmap( in_bmp_f )
+        bmp.pixels(
+            lambda p: print( f"{hex( p )} ", end='' ) \
+                if 0 < p else print( '    ', end='' ),
+            lambda y: print( '' ) )
 
 if '__main__' == __name__:
     main()
