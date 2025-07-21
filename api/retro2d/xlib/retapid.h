c60d7c04936cb7997814e327adbdbe193382cacb
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-07-20 21:07:46 -0400

    Comitting work on new xlib API.

---
+++ api/retro2d/xlib/retapid.h
@@ -0,0 +1,133 @@
+
+#ifndef RETPLTD_H
+#define RETPLTD_H
+
+/**
+ * \file retpltd.h
+ * \brief RetroFlat platform definition header.
+ */
+
+#define RETROPLAT_PRESENT 1
+
+#define RETROFLAT_SOFT_VIEWPORT
+
+#define RETROFLAT_SOFT_SHAPES
+
+#define RETROFLAT_SOFT_LINES
+
+#include <time.h>
+#include <X11/Xutil.h>
+
+/**
+ * \addtogroup maug_retroflt_bitmap
+ * \{
+ */
+
+/**
+ * \brief Platform-specific bitmap structure. retroflat_bitmap_ok() can be
+ *        used on a pointer to it to determine if a valid bitmap is loaded.
+ *
+ * Please see the \ref maug_retroflt_bitmap for more information.
+ */
+struct RETROFLAT_BITMAP {
+   /*! \brief Size of the bitmap structure, used to check VDP compatibility. */
+   size_t sz;
+   /*! \brief Platform-specific bitmap flags. */
+   uint8_t flags;
+   XImage* img;
+   char* bits;
+   int bits_sz;
+};
+
+/**
+ * \relates RETROFLAT_BITMAP
+ * \brief Check to see if a bitmap is loaded.
+ */
+#  define retroflat_bitmap_ok( bitmap ) (NULL != (bitmap)->img)
+
+/**
+ * \relates RETROFLAT_BITMAP
+ * \brief Check to see if a bitmap is currently locked.
+ */
+#  define retroflat_bitmap_locked( bitmap ) (0)
+
+/**
+ * \relates RETROFLAT_BITMAP
+ * \brief Get the width of this bitmap using underlying mechanisms.
+ * \warn The bitmap must be valid!
+ */
+#  define retroflat_bitmap_w( bitmap ) ((bitmap)->img->width)
+
+/**
+ * \relates RETROFLAT_BITMAP
+ * \brief Get the height of this bitmap using underlying mechanisms.
+ * \warn The bitmap must be valid!
+ */
+#  define retroflat_bitmap_h( bitmap ) ((bitmap)->img->height)
+
+/*! \} */ /* maug_retroflt_bitmap */
+
+/*! \brief Get the current screen width in pixels. */
+#  define retroflat_screen_w() (g_retroflat_state->screen_v_w)
+
+/*! \brief Get the current screen height in pixels. */
+#  define retroflat_screen_h() (g_retroflat_state->screen_v_h)
+
+/*! \brief Get the direct screen buffer or the VDP buffer if a VDP is loaded. */
+#  define retroflat_screen_buffer() (&(g_retroflat_state->buffer))
+
+/*! \brief Lock a surface for pixel drawing if needed. */
+#  define retroflat_px_lock( bmp )
+
+/*! \brief Release a surface for pixel drawing if needed. */
+#  define retroflat_px_release( bmp )
+
+/**
+ * \brief This should be called in order to quit a program using RetroFlat.
+ * \param retval The return value to pass back to the operating system.
+ */
+#  define retroflat_quit( retval_in ) \
+      debug_printf( 1, "quit called, retval: %d", retval_in ); \
+      g_retroflat_state->retroflat_flags &= ~RETROFLAT_FLAGS_RUNNING; \
+      g_retroflat_state->retval = retval_in;
+
+/*! \brief Defined for backward-compatibility with Allegro. */
+#  define END_OF_MAIN()
+
+/**
+ * \addtogroup maug_retroflt_drawing
+ * \{
+ * 
+ * \addtogroup maug_retroflt_color RetroFlat Colors
+ * \brief Color definitions RetroFlat is aware of, for use with the
+ *        \ref maug_retroflt_drawing.
+ *
+ * The precise type and values of these constants vary by platform.
+ *
+ * \{
+ */
+
+/*! \brief Example, should be replaced with platform-specific type. */
+typedef uint32_t RETROFLAT_COLOR_DEF;
+
+/*! \} */ /* maug_retroflt_color */
+
+/*! \} */ /* maug_retroflt_drawing */
+
+struct RETROFLAT_PLATFORM_ARGS {
+   /*! \brief Example field to prevent empty struct. */
+   uint8_t flags;
+};
+
+struct RETROFLAT_PLATFORM {
+   /*! \brief Example field to prevent empty struct. */
+   uint8_t flags;
+   int screen;
+   Window window;
+   Display* display;
+   Visual* visual;
+   uint32_t s_launch;
+};
+
+#endif /* !RETPLTD_H */
+
---
+++ api/retro2d/xlib/retapif.h
@@ -0,0 +1,378 @@
+
+#ifndef RETPLTF_H
+#define RETPLTF_H
+
+static MERROR_RETVAL retroflat_init_platform(
+   int argc, char* argv[], struct RETROFLAT_ARGS* args
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+   struct timespec spec;
+   time_t tm;
+
+   /* TODO: Add some flexibility for simulating lower-color platforms. */
+   g_retroflat_state->screen_colors = 16;
+
+   g_retroflat_state->platform.display = XOpenDisplay( NULL );
+   if( NULL == g_retroflat_state->platform.display ) {
+      error_printf( "unable to open display" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+
+   g_retroflat_state->platform.screen = DefaultScreen(
+      g_retroflat_state->platform.display );
+
+   g_retroflat_state->platform.window = XCreateSimpleWindow(
+      g_retroflat_state->platform.display,
+      RootWindow(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      100, 100,
+      g_retroflat_state->screen_w,
+      g_retroflat_state->screen_h,
+      1,
+      BlackPixel(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      WhitePixel(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ) );
+
+   g_retroflat_state->platform.visual = DefaultVisual(
+      g_retroflat_state->platform.display,
+      g_retroflat_state->platform.screen );
+
+   /* TODO: Check? */
+
+   /* Show the window. */
+   XMapWindow(
+      g_retroflat_state->platform.display,
+      g_retroflat_state->platform.window );
+
+   /* Create the buffer. */
+   g_retroflat_state->buffer.bits_sz =
+      g_retroflat_state->screen_w * g_retroflat_state->screen_h * 4;
+   g_retroflat_state->buffer.bits = calloc(
+      g_retroflat_state->screen_w * g_retroflat_state->screen_h, 4 );
+   if( NULL == g_retroflat_state->buffer.bits ) {
+      error_printf( "coult not allocate buffer bitmap!" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+   g_retroflat_state->buffer.img = XCreateImage(
+      g_retroflat_state->platform.display, g_retroflat_state->platform.visual, 
+      DefaultDepth(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      ZPixmap,
+      0,
+      g_retroflat_state->buffer.bits,
+      g_retroflat_state->screen_w,
+      g_retroflat_state->screen_h, 32, 0 );
+   if( NULL == g_retroflat_state->buffer.img ) {
+      error_printf( "coult not allocate buffer image!" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+
+   clock_gettime( CLOCK_MONOTONIC, &spec );
+   g_retroflat_state->platform.s_launch = spec.tv_sec;
+
+   srand( (unsigned int)time( &tm ) );
+
+#  define RETROFLAT_COLOR_TABLE_XLIB( idx, name_l, name_u, rd, gd, bd, cgac, cgad ) \
+      g_retroflat_state->palette[idx] = \
+         (((rd) & 0xff) << 16) | (((gd) & 0xff) << 8) | ((bd) & 0xff);
+   RETROFLAT_COLOR_TABLE( RETROFLAT_COLOR_TABLE_XLIB )
+
+cleanup:
+
+   return retval;
+}
+
+/* === */
+
+void retroflat_shutdown_platform( MERROR_RETVAL retval ) {
+   if( NULL != g_retroflat_state->buffer.img ) {
+      XDestroyImage( g_retroflat_state->buffer.img );
+   }
+   /* if( NULL != g_buffer_bits ) {
+      free( g_buffer_bits );
+   } */
+   XCloseDisplay( g_retroflat_state->platform.display );
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_loop(
+   retroflat_loop_iter frame_iter, retroflat_loop_iter loop_iter, void* data
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   /* Just skip to the generic loop. */
+   retval = retroflat_loop_generic( frame_iter, loop_iter, data );
+
+   /* This should be set by retroflat_quit(). */
+   return retval;
+}
+
+/* === */
+
+void retroflat_message(
+   uint8_t flags, const char* title, const char* format, ...
+) {
+   char msg_out[RETROFLAT_MSG_MAX + 1];
+   va_list vargs;
+
+   memset( msg_out, '\0', RETROFLAT_MSG_MAX + 1 );
+   va_start( vargs, format );
+   maug_vsnprintf( msg_out, RETROFLAT_MSG_MAX, format, vargs );
+
+   /* TODO */
+#  pragma message( "warning: message not implemented" )
+
+   va_end( vargs );
+}
+
+/* === */
+
+void retroflat_set_title( const char* format, ... ) {
+   char title[RETROFLAT_TITLE_MAX + 1];
+   va_list vargs;
+
+   /* Build the title. */
+   va_start( vargs, format );
+   memset( title, '\0', RETROFLAT_TITLE_MAX + 1 );
+   maug_vsnprintf( title, RETROFLAT_TITLE_MAX, format, vargs );
+
+   /* TODO */
+#  pragma message( "warning: set_title not implemented" )
+
+   va_end( vargs );
+}
+
+/* === */
+
+retroflat_ms_t retroflat_get_ms() {
+   struct timespec spec;
+   uint32_t ms_out = 0,
+      ms_launch_delta = 0;
+   
+   clock_gettime( CLOCK_MONOTONIC, &spec );
+
+   /* Get the seconds since program launched. Multiply by 1000, so we want a
+    * smaller number than seconds since the epoch. */
+   ms_launch_delta = spec.tv_sec - g_retroflat_state->platform.s_launch;
+   ms_out += ms_launch_delta * 1000;
+   ms_out += spec.tv_nsec / 1000000;
+
+   return ms_out;
+}
+
+/* === */
+
+uint32_t retroflat_get_rand() {
+   return rand();
+}
+
+/* === */
+
+int retroflat_draw_lock( struct RETROFLAT_BITMAP* bmp ) {
+   int retval = RETROFLAT_OK;
+
+   /* TODO */
+#  pragma message( "warning: draw_lock not implemented" )
+
+   return retval;
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_draw_release( struct RETROFLAT_BITMAP* bmp ) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   XPutImage(
+      g_retroflat_state->platform.display,
+      g_retroflat_state->platform.window,
+      DefaultGC(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      g_retroflat_state->buffer.img,
+      0, 0, 0, 0,
+      g_retroflat_state->screen_w, g_retroflat_state->screen_h );
+
+   return retval;
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_load_bitmap(
+   const char* filename, struct RETROFLAT_BITMAP* bmp_out, uint8_t flags
+) {
+   char filename_path[RETROFLAT_PATH_MAX + 1];
+   MERROR_RETVAL retval = MERROR_OK;
+
+   assert( NULL != bmp_out );
+   maug_mzero( bmp_out, sizeof( struct RETROFLAT_BITMAP ) );
+   retval = retroflat_build_filename_path(
+      filename, filename_path, RETROFLAT_PATH_MAX + 1, flags );
+   maug_cleanup_if_not_ok();
+   debug_printf( 1, "retroflat: loading bitmap: %s", filename_path );
+
+   /* TODO */
+#  pragma message( "warning: load_bitmap not implemented" )
+
+cleanup:
+
+   return retval;
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_create_bitmap(
+   size_t w, size_t h, struct RETROFLAT_BITMAP* bmp_out, uint8_t flags
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   maug_mzero( bmp_out, sizeof( struct RETROFLAT_BITMAP ) );
+
+   bmp_out->sz = sizeof( struct RETROFLAT_BITMAP );
+
+   /* Create the buffer. */
+   bmp_out->bits_sz = w * h * 4;
+   bmp_out->bits = calloc( w * h, 4 );
+   if( NULL == bmp_out->bits ) {
+      error_printf( "coult not allocate bitmap bits!" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+   bmp_out->img = XCreateImage(
+      g_retroflat_state->platform.display, g_retroflat_state->platform.visual, 
+      DefaultDepth(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      ZPixmap,
+      0,
+      bmp_out->bits,
+      w, h, 32, 0 );
+   if( NULL == bmp_out->img ) {
+      error_printf( "coult not allocate bitmap image!" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+
+cleanup:
+
+   return retval;
+}
+
+/* === */
+
+void retroflat_destroy_bitmap( struct RETROFLAT_BITMAP* bmp ) {
+
+   /* TODO */
+#  pragma message( "warning: destroy_bitmap not implemented" )
+
+}
+
+/* === */
+
+MERROR_RETVAL retroflat_blit_bitmap(
+   struct RETROFLAT_BITMAP* target, struct RETROFLAT_BITMAP* src,
+   size_t s_x, size_t s_y, int16_t d_x, int16_t d_y, size_t w, size_t h,
+   int16_t instance
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+   Pixmap target_pxm = 0;
+   size_t t_w, t_h;
+
+   assert( NULL != src );
+
+   if( NULL == target ) {
+      target = retroflat_screen_buffer();
+   }
+
+   if(
+      RETROFLAT_FLAGS_BITMAP_RO == (RETROFLAT_FLAGS_BITMAP_RO & target->flags)
+   ) {
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+
+   t_w = target->img->width;
+   t_h = target->img->height;
+
+   /* Convert the target into a client pixmap so we can blit to it. */
+   target_pxm = XCreatePixmap(
+      g_retroflat_state->platform.display,
+      g_retroflat_state->platform.window,
+      t_w, t_h,
+      DefaultDepth(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ) );
+
+   if( 0 == target_pxm ) {
+      error_printf( "could not create target pixmap!" );
+      retval = MERROR_GUI;
+      goto cleanup;
+   }
+
+   /* Perform the actual blit. */
+   XPutImage(
+      g_retroflat_state->platform.display,
+      target_pxm,
+      DefaultGC(
+         g_retroflat_state->platform.display,
+         g_retroflat_state->platform.screen ),
+      src->img,
+      s_x, s_y, d_x, d_y, w, h );
+
+   /* Replace the old target image with the newly-blitted target pixmap. */
+   XDestroyImage( target->img );
+   target->img = XGetImage(
+      g_retroflat_state->platform.display,
+      target_pxm,
+      0, 0, t_w, t_h, AllPlanes, ZPixmap );
+
+cleanup:
+
+   if( 0 < target_pxm ) {
+      XFreePixmap( g_retroflat_state->platform.display, target_pxm );
+   }
+
+   return retval;
+}
+
+/* === */
+
+void retroflat_px(
+   struct RETROFLAT_BITMAP* target, const RETROFLAT_COLOR color_idx,
+   size_t x, size_t y, uint8_t flags
+) {
+   if( RETROFLAT_COLOR_NULL == color_idx ) {
+      return;
+   }
+
+   if( NULL == target ) {
+      target = retroflat_screen_buffer();
+   }
+
+   if(
+      RETROFLAT_FLAGS_BITMAP_RO == (RETROFLAT_FLAGS_BITMAP_RO & target->flags)
+   ) {
+      return;
+   }
+
+   retroflat_constrain_px( x, y, target, return );
+
+   XPutPixel( target->img, x, y, g_retroflat_state->palette[color_idx] );
+}
+
+/* === */
+
+void retroflat_resize_v() {
+   /* Platform does not support resizing. */
+}
+
+#endif /* !RETPLTF_H */
+
