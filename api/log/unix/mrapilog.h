1f97d2fd2f339ab0ce161ce2f25687687fe5a840
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-07-31 21:28:19 -0400

    Use functions in UNIX log API to maintain access to global handle.

--- api/log/unix/mrapilog.h
+++ api/log/unix/mrapilog.h
@@ -27,34 +27,65 @@
 #  define DEBUG_THRESHOLD 1
 #endif /* !DEBUG_THRESHOLD */
 
-#  define internal_debug_printf( lvl, ... ) if( NULL != LOG_ERR_TARGET && lvl >= DEBUG_THRESHOLD ) { fprintf( LOG_STD_TARGET, "(%d) " __FILE__ ": %d: ", lvl, LINE_NUMBER() ); fprintf( LOG_STD_TARGET, __VA_ARGS__ ); fprintf( LOG_STD_TARGET, NEWLINE_STR ); fflush( LOG_STD_TARGET ); }
+void _internal_debug_printf(
+  int lvl, const char* src, size_t line, const char* fmt, ... );
 
-#  define internal_error_printf( ... ) if( NULL != LOG_ERR_TARGET ) { fprintf( LOG_ERR_TARGET, "(E) " __FILE__ ": %d: ", LINE_NUMBER() ); fprintf( LOG_ERR_TARGET, __VA_ARGS__ ); fprintf( LOG_ERR_TARGET, NEWLINE_STR ); fflush( LOG_ERR_TARGET ); }
+#  define debug_printf( lvl, ... ) \
+      _internal_debug_printf( lvl, __FILE__, __LINE__, __VA_ARGS__ )
 
-#  define debug_printf( lvl, ... ) internal_debug_printf( lvl, __VA_ARGS__ )
+#  define error_printf( ... ) \
+      _internal_debug_printf( -1, __FILE__, __LINE__, __VA_ARGS__ )
 
-#  define error_printf( ... ) internal_error_printf( __VA_ARGS__ )
+#  define size_printf( lvl, name, sz ) \
+      _internal_debug_printf( lvl, __FILE__, __LINE__, \
+         name " size is " SIZE_T_FMT " bytes", (sz) );
 
-#  define size_printf( lvl, name, sz ) internal_debug_printf( lvl, name " size is " SIZE_T_FMT " bytes", (sz) );
-
-#  define size_multi_printf( lvl, name, sz, max ) internal_debug_printf( lvl, "single " name " size is " SIZE_T_FMT " bytes, " name " array size is " SIZE_T_FMT " bytes", (sz), ((sz) * (max)) );
+#  define size_multi_printf( lvl, name, sz, max ) \
+      _internal_debug_printf( lvl, __FILE__, __LINE__, \
+         "single " name " size is " SIZE_T_FMT " bytes, " name " array size is " SIZE_T_FMT " bytes", (sz), ((sz) * (max)) );
 
 #  ifdef LOG_TO_FILE
-#     ifndef MFILE_C
 extern FILE* SEG_MGLOBAL g_log_file;
-#     endif /* MFILE_C */
-#     define logging_init() g_log_file = fopen( LOG_FILE_NAME, "w" );
-#     define logging_shutdown() fclose( g_log_file );
-#  else
-#     define logging_init()
-#     define logging_shutdown()
 #  endif /* LOG_TO_FILE */
+
+void logging_init();
+
+void logging_shutdown();
 
 #elif defined( MFILE_C )
 
 #  ifdef LOG_TO_FILE
 FILE* SEG_MGLOBAL g_log_file = NULL;
 #  endif /* LOG_TO_FILE */
+
+void logging_init() {
+#  ifdef LOG_TO_FILE
+   g_log_file = fopen( LOG_FILE_NAME, "w" );
+#  endif /* LOG_FILE_NAME */
+}
+
+/* === */
+
+void logging_shutdown() {
+   fclose( g_log_file );
+}
+
+/* === */
+
+void _internal_debug_printf(
+   int lvl, const char* src, size_t line, const char* fmt, ...
+) {
+   va_list args;
+
+   if( NULL != LOG_ERR_TARGET && lvl >= DEBUG_THRESHOLD ) {
+      fprintf( LOG_STD_TARGET, "(%d) " __FILE__ ": %d: ", lvl, line );
+      va_start( args, fmt );
+      vfprintf( LOG_STD_TARGET, fmt, args );
+      va_end( args );
+      fprintf( LOG_STD_TARGET, NEWLINE_STR );
+      fflush( LOG_STD_TARGET );
+   }
+}
 
 #endif /* !MAUG_API_LOG_H_DEFS */
 
