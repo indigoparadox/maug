a330f67742ce8061f85d849e1d5c697f2e156cc9
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-08-06 21:27:18 -0400

    Add Wii sound stub.

---
+++ api/sound/wii/retapis.h
@@ -0,0 +1,133 @@
+
+#if !defined( RETPLTS_H_DEFS )
+#define RETPLTS_H_DEFS
+
+struct RETROFLAT_SOUND_ARGS {
+   uint8_t flags;
+};
+
+struct RETROFLAT_SOUND_STATE {
+   uint8_t flags;
+};
+
+#elif defined( RETROFLT_C )
+
+MERROR_RETVAL retrosnd_cli_rsd(
+   const char* arg, ssize_t arg_c, struct RETROFLAT_ARGS* args
+) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   /* TODO */
+
+   return retval;
+}
+
+/* === */
+
+MERROR_RETVAL retrosnd_init( struct RETROFLAT_ARGS* args ) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+   assert( 2 <= sizeof( MERROR_RETVAL ) );
+
+#  pragma message( "warning: init not implemented" )
+
+   return retval;
+}
+
+/* === */
+
+void retrosnd_midi_set_sf_bank( const char* filename_in ) {
+#  pragma message( "warning: set_sf_bank not implemented" )
+}
+
+/* === */
+
+void retrosnd_midi_set_voice( uint8_t channel, uint8_t voice ) {
+
+   if(
+      RETROSND_FLAG_INIT !=
+      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
+   ) {
+      return;
+   }
+
+#  pragma message( "warning: set_voice not implemented" )
+}
+
+/* === */
+
+void retrosnd_midi_set_control( uint8_t channel, uint8_t key, uint8_t val ) {
+
+   if(
+      RETROSND_FLAG_INIT !=
+      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
+   ) {
+      return;
+   }
+
+#  pragma message( "warning: set_control not implemented" )
+}
+
+/* === */
+
+void retrosnd_midi_note_on( uint8_t channel, uint8_t pitch, uint8_t vel ) {
+
+   if(
+      RETROSND_FLAG_INIT !=
+      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
+   ) {
+      return;
+   }
+
+#  pragma message( "warning: note_on not implemented" )
+
+}
+
+/* === */
+
+void retrosnd_midi_note_off( uint8_t channel, uint8_t pitch, uint8_t vel ) {
+
+   if(
+      RETROSND_FLAG_INIT !=
+      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
+   ) {
+      return;
+   }
+
+#  pragma message( "warning: note_off not implemented" )
+
+}
+
+/* === */
+
+MERROR_RETVAL retrosnd_midi_play_smf( const char* filename ) {
+   MERROR_RETVAL retval = MERROR_OK;
+
+#  pragma message( "warning: midi_play_smf not implemented" )
+
+   return retval;
+}
+
+/* === */
+
+uint8_t retrosnd_midi_is_playing_smf() {
+#  pragma message( "warning: midi_play_smf not implemented" )
+   return 1;
+}
+
+/* === */
+
+void retrosnd_shutdown() {
+
+   if(
+      RETROSND_FLAG_INIT !=
+      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
+   ) {
+      return;
+   }
+
+#  pragma message( "warning: shutdown not implemented" )
+}
+
+#endif /* !RETPLTS_H_DEFS || RETROFLT_C */
+
