fa6623b6cb340fb77afc02d1bb0243c1cccd6e91
Forgejo <forgejo@interfinitydynamics.info>
2025-08-09 21:36:14 +0000

    Comitting docs.

---
+++ search/groups_4.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['maug_20file_20format_20api_0',['Maug File Format API',['../group__maug__fmt.html',1,'']]],
+  ['maug_20file_20format_3a_20bitmap_1',['Maug File Format: Bitmap',['../group__maug__fmt__bmp.html',1,'']]],
+  ['maug_5fretro3d_5futil_2',['Maug_retro3d_util',['../group__maug__retro3d__util.html',1,'']]],
+  ['maug_5fretroflt_5fcolor_3',['Maug_retroflt_color',['../group__maug__retroflt__color.html',1,'']]],
+  ['mlisp_20abstract_20syntax_20tree_20parser_4',['MLISP Abstract Syntax Tree Parser',['../group__mlisp__parser.html',1,'']]],
+  ['mlisp_20execution_20stack_5',['MLISP Execution Stack',['../group__mlisp__stack.html',1,'']]],
+  ['mlisp_20interpreter_6',['MLISP Interpreter',['../group__mlisp.html',1,'']]],
+  ['mlisp_20types_7',['MLISP Types',['../group__mlisp__types.html',1,'']]]
+];
---
+++ search/variables_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bpp_0',['bpp',['../structMFMT__STRUCT__BMPINFO.html#a6761f9c031d0afe92bb21ebdf33ee5cf',1,'MFMT_STRUCT_BMPINFO']]],
+  ['buffer_1',['buffer',['../group__maug__retroflt.html#ga940f28bcdcbe48c35d52d901f8787c50',1,'RETROFLAT_STATE']]]
+];
