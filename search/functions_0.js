bcc999d080ff3dfa387d4cfb454f3d7f083c5f54
Forgejo <forgejo@interfinitydynamics.info>
2025-08-30 16:09:47 +0000

    Committing docs.

---
+++ search/functions_0.js
@@ -0,0 +1,23 @@
+var searchData=
+[
+  ['maug_5fadd_5farg_0',['maug_add_arg',['../group__maug__cli.html#ga123030ccd968ae3f5669552250a9fc66',1,'marge.h']]],
+  ['mdata_5fvector_5falloc_1',['mdata_vector_alloc',['../group__mdata__vector.html#gadbb2f9f55756645e0a28caf76c676e4c',1,'mdata.h']]],
+  ['mdata_5fvector_5fappend_2',['mdata_vector_append',['../group__mdata__vector.html#ga4f9acb8c4e8000c024728d0a664ce2f3',1,'MDATA_VECTOR']]],
+  ['mdata_5fvector_5fget_5fvoid_3',['mdata_vector_get_void',['../group__mdata__vector.html#ga181f89aec4706df538ec18c623ae21a9',1,'MDATA_VECTOR']]],
+  ['mdata_5fvector_5fremove_4',['mdata_vector_remove',['../group__mdata__vector.html#ga9a86595990aaff9dc7ae3abe6a05a665',1,'MDATA_VECTOR']]],
+  ['mfile_5fclose_5',['mfile_close',['../group__maug__mfile.html#gabf28a1a9cb4595b757aa79312749318c',1,'mfile.h']]],
+  ['mfile_5flock_5fbuffer_6',['mfile_lock_buffer',['../group__maug__mfile.html#gac13df68934cfc9433fbbc057802eefec',1,'mfile.h']]],
+  ['mfile_5fopen_5fread_7',['mfile_open_read',['../group__maug__mfile.html#gaaa053ea8ff41c0b9ecb854d70103221f',1,'mfile.h']]],
+  ['mfmt_5fdecode_5frle_8',['mfmt_decode_rle',['../group__maug__fmt.html#ga4fffc8c6ce282e9198946a40dbe8e70c',1,'mfmt.h']]],
+  ['mfmt_5fget_5fpx_5fptr_9',['mfmt_get_px_ptr',['../group__maug__fmt.html#ga425bf1c02ecfe01b5aff5aba43ace273',1,'mfmt.h']]],
+  ['mfmt_5fread_5fbmp_5fpx_10',['mfmt_read_bmp_px',['../group__maug__fmt.html#ga3aed5af79845ff84c2b5180728d6a883',1,'mfmt.h']]],
+  ['mfmt_5fread_5fbmp_5fpx_5fcb_11',['mfmt_read_bmp_px_cb',['../group__maug__fmt.html#gaa103a8b490919b53827c9df1fea563ae',1,'mfmt.h']]],
+  ['mlisp_5fast_5fdump_12',['mlisp_ast_dump',['../group__mlisp__parser.html#ga4f9ef4a7321a2113017245a17055b6dd',1,'mlispp.h']]],
+  ['mlisp_5fenv_5fdump_13',['mlisp_env_dump',['../group__mlisp.html#ga0c93c38d01cb70fb289aec1793e4880d',1,'mlispe.h']]],
+  ['mlisp_5fenv_5fget_5fstrpool_14',['mlisp_env_get_strpool',['../group__mlisp.html#gaed9a4267b7ca9939478969e527e95439',1,'mlispe.h']]],
+  ['mlisp_5fexec_5fset_5fglobal_5fenv_15',['mlisp_exec_set_global_env',['../group__mlisp.html#gabc427150987fd7eb3a71841fa93607a0',1,'mlispe.h']]],
+  ['mlisp_5fstack_5fdump_16',['mlisp_stack_dump',['../group__mlisp__stack.html#ga28d22a4b32bd9190845edb1c378bac7b',1,'mlispe.h']]],
+  ['mlisp_5fstack_5fpop_5fex_17',['mlisp_stack_pop_ex',['../group__mlisp__stack.html#gacd8db04e907ff1acf7ecf2e1276beebd',1,'mlispe.h']]],
+  ['mlisp_5fstep_18',['mlisp_step',['../group__mlisp.html#ga733fc40dc341bf42a2afea5e6b989672',1,'mlispe.h']]],
+  ['mlisp_5fstep_5flambda_19',['mlisp_step_lambda',['../group__mlisp.html#gab0f38453c8a626d977538798d57d87fb',1,'mlispe.h']]]
+];
---
+++ search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['env_0',['env',['../structMLISP__EXEC__STATE.html#af2ae447361c047d672be9490c1d34ba2',1,'MLISP_EXEC_STATE::env()'],['../structMLISP__PARSER.html#a743cf11fb615af6a7a45332830feb286',1,'MLISP_PARSER::env()']]]
+];
