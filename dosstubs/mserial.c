e77d2991bf90481c6f95adef8e18350fec88b4d1
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-09-01 19:20:30 -0400

    Add DOS stub for serial.

---
+++ dosstubs/mserial.c
@@ -0,0 +1,13 @@
+
+#ifndef MAUG_NO_MSERIAL
+#  define MSERIAL_C
+#  include <maug.h>
+#  include <mjson.h>
+#  include <retroflt.h>
+#  ifndef RETROFLAT_NO_RETROGXC
+#     include <retrogxc.h>
+#  endif
+#  include <retrotil.h>
+#  include <mserial.h>
+#endif /* !MAUG_NO_MSERIAL */
+
