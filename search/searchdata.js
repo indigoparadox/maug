b8adfb4af189fbf9c8e7e6b62961bc8c49a4015a
Forgejo <forgejo@interfinitydynamics.info>
2025-12-07 14:37:35 +0000

    Committing docs.

---
+++ search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "<abcdefhilmnprstvw",
+  1: "mr",
+  2: "mr",
+  3: "mr",
+  4: "abcdefhilmnprstvw",
+  5: "mr",
+  6: "mr",
+  7: "cdeimrs",
+  8: "<cmrst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "related",
+  7: "groups",
+  8: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Friends",
+  7: "Modules",
+  8: "Pages"
+};
+
