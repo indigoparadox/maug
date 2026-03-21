8c88e03e41777e5eab6d05ece21b6b98ec34c1ef
indigoparadox <indigo.repo@interfinitydynamics.info>
2026-03-20 20:10:19 -0400

    Cleanups and fixes for new mem API and debugger.

--- README.md
+++ README.md
@@ -167,39 +167,6 @@ Includes common types from stdint.h, guaranteed to be of the named size:
 
 When using this header, do not include stdint.h. This header will include it if the current platform supports it. Otherwise, it will improvise.
 
-### uprintf
-
-`#include <uprintf.h>`
-
-An augmented printf for tracing, debugging, and error reporting.
-
-To use, include uprintf.h and call `logging\_init()` in program startup.
-
-Be sure to call `logging\_shutdown()` on program cleanup.
-
-Logging can then be performed by calling:
-
- * `debug\_printf( lvl, format, ... )` - Prints debug message.
-   * `lvl` - Immediacy level of debug message. See DEBUG\_THRESHOLD below.
-   * `format` - `printf()`-style format string.
-   * `...` - Variable arguments to `format`.
- * `error\_printf( format, ... )` - Prints to stderr.
-   * `format` - `printf()`-style format string.
-   * `...` - Variable arguments to `format`.
-
-uprintf's behavior can be modified by defining the following before inclusion:
-
-| Define            | Description                                             |
-| ------------------|---------------------------------------------------------|
-| platform\_file    | Type to use instead of stdlib's FILE for log ptr.       |
-| platform\_fopen   | Function to use instead of fopen() to create a log ptr. |
-| platform\_fclose  | Function to close log ptr.                              |
-| platform\_fflush  | Function to flush log ptr to buffer.                    |
-| platform\_fprintf | Function to print to log ptr with a format string.      |
-| DEBUG\_THRESHOLD  | debug\_printf() w/ this or higher lvl will be displayed.|
-| DEBUG\_LOG        | If defined, debug will be output to LOG\_FILE\_NAME.    |
-| ANCIENT\_C        | Use static functions where vararg macros unsupported.   |
-
 ## MVFS
 
 The maug virtual filesystem is a "special case" file API available on platforms that may have lots of RAM but no supported filesystem. It allows for the compiling of resources into programs.
--- api/mem/unix/mmem.h
+++ api/mem/unix/mmem.h
@@ -8,7 +8,7 @@
  * \file mmem.h
  */
 
-#  if !defined( DOCUMENTATION ) && MMEM_DEFS_CT_MAX
+#  if !defined( DOCUMENTATION ) && !defined( MMEM_DEFS_CT_MAX )
 #     define MMEM_DEFS_CT_MAX 100
 #  endif /* !MMEM_DEFS_CT_MAX */
 
@@ -22,9 +22,11 @@ struct MMEM_DEF {
 #  if !defined( DOCUMENTATION ) && MMEM_TRACE_LVL > 0
 void debug_mmem_realloc( void* handle, void* new_handle, int sz );
 void debug_mmem_alloc( void* handle, int sz );
+void debug_mmem_free( void* handle );
 #  else
 #     define debug_mmem_realloc( handle, new_handle, sz )
 #     define debug_mmem_alloc( handle, sz )
+#     define debug_mmem_free( handle )
 #  endif /* MMEM_TRACE_LVL */
 
 #  define maug_malloc_test( handle, nmemb, sz ) \
@@ -42,7 +44,10 @@ void debug_mmem_alloc( void* handle, int sz );
 
 #  define maug_mcpy( ptr_dest, ptr_src, sz ) memcpy( ptr_dest, ptr_src, sz )
 
-#  define maug_mfree( handle ) free( handle ); handle = NULL;
+#  define maug_mfree( handle ) \
+      debug_mmem_free( handle ); \
+      free( handle ); \
+      handle = NULL;
 
 #  define maug_mlock( handle, ptr ) ptr = handle; handle = (void*)0xDEADBEEF;
 
@@ -75,32 +80,95 @@ struct MMEM_DEF g_mmem_defs[MMEM_DEFS_CT_MAX];
 int g_mmem_defs_ct = 0;
 void* g_mmem_alloc_ptr = NULL;
 
+void debug_mmem_print_total() {
+   size_t i = 0;
+   int sz_total = 0;
+   char prefix = ' ';
+
+#  ifdef MMEM_DUMP_ENABLED
+   printf( "---\n" );
+#  endif /* MMEM_DUMP_ENABLED */
+   for( i = 0 ; g_mmem_defs_ct > i ; i++ ) {
+#  ifdef MMEM_DUMP_ENABLED
+      printf(
+         SIZE_T_FMT ". %p (%d bytes)\n",
+         i, g_mmem_defs[i].handle, g_mmem_defs[i].sz );
+#  endif /* MMEM_DUMP_ENABLED */
+      sz_total += g_mmem_defs[i].sz;
+   }
+#  ifdef MMEM_DUMP_ENABLED
+   printf( "---\n" );
+#  endif /* MMEM_DUMP_ENABLED */
+
+   if( 1024 < sz_total ) {
+      prefix = 'k';
+      sz_total /= 1024;
+   }
+   if( 1024 < sz_total ) {
+      prefix = 'm';
+      sz_total /= 1024;
+   }
+   if( 1024 < sz_total ) {
+      prefix = 'g';
+      sz_total /= 1024;
+   }
+
+   printf( "%d %cbytes in %d allocs\n", sz_total, prefix, g_mmem_defs_ct );
+}
+
+/* === */
+
+void debug_mmem_free( void* handle ) {
+   size_t i = 0 ;
+
+   for( i = 0 ; g_mmem_defs_ct > i ; i++ ) {
+      if( handle == g_mmem_defs[i].handle ) {
+         memmove(
+            &(g_mmem_defs[i]),
+            &(g_mmem_defs[i + 1]),
+            (g_mmem_defs_ct - (i + 1)) * sizeof( struct MMEM_DEF ) );
+         g_mmem_defs_ct--;
+         printf( "alloc %p (" SIZE_T_FMT ") freed, ", handle, i );
+         debug_mmem_print_total();
+         return;
+      }
+   }
+   printf( "attempted to free invalid alloc %p!\n", handle );
+}
+
+/* === */
+
 void debug_mmem_alloc( void* handle, int sz ) {
    g_mmem_defs[g_mmem_defs_ct].handle = handle;
    g_mmem_defs[g_mmem_defs_ct].sz = sz;
    if( g_mmem_defs_ct + 1 < MMEM_DEFS_CT_MAX ) {
       g_mmem_defs_ct++;
    }
+   printf(
+      "handle %p (%d) alloced as sz: %d, ",
+      handle, g_mmem_defs_ct - 1, sz );
+   debug_mmem_print_total();
 }
 
 /* === */
 
 void debug_mmem_realloc( void* handle, void* new_handle, int sz ) {
    size_t i = 0 ;
-   /* int old_sz = 0; */
 
    for( i = 0 ; g_mmem_defs_ct > i ; i++ ) {
       if( handle == g_mmem_defs[i].handle ) {
          g_mmem_defs[i].handle = new_handle;
-         /*
-         debug_printf( MMEM_TRACE_LVL,
-            "pointer %p changed to %p, size changed from %d to %d",
-            handle, new_handle, g_mmem_defs[i].sz, sz );
-         */
+         printf(
+            "alloc %p (" SIZE_T_FMT
+               ") changed to %p, size changed from %d to %d, ",
+            handle, i, new_handle, g_mmem_defs[i].sz, sz );
+         debug_mmem_print_total();
          g_mmem_defs[i].sz = sz;
          return;
       }
    }
+
+   printf( "attempted to realloc invalid alloc %p!\n", handle );
 }
 
 #  endif /* MMEM_TRACE_LVL */
