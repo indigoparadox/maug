7e620d5d8b1aae1dc93f34657ff46e873cf6b670
indigoparadox <indigo.repo@interfinitydynamics.info>
2026-04-01 19:34:40 -0400

    Turn CSS merge into a function.

--- src/mcssmerg.h
+++ src/mcssmerg.h
@@ -18,6 +18,97 @@ void mcssmerge_styles(
  * stub segment to become too large because of its use of the preprocessor.
  */
 
+void mcssmerge_merge_prop(
+   int p_id,
+   const char* prop_name,
+   size_t prop_sz,
+   size_t tag_type,
+   void* parent_prop, uint8_t* parent_prop_flags,
+   void* effect_prop, uint8_t* effect_prop_flags,
+   void* tag_prop, uint8_t* tag_prop_flags
+) {
+   if(
+      (
+         /* Only do inheritence for some special cases.
+          * e.g. We don't want to inherit width/height/X/etc! */
+         mcss_prop_is_heritable( p_id )
+      ) && (NULL != parent_prop && (
+         (
+            NULL != tag_prop &&
+            /* Parent is important and new property isn't. */
+            mcss_prop_is_active_flag( *parent_prop, IMPORTANT ) &&
+            /* TODO: Is not active OR important? */
+            !mcss_prop_is_important( *tag_prop ) &&
+            !mcss_prop_is_important( *effect_prop )
+         ) || (
+            NULL != tag_prop &&
+            /* New property is not active. */
+            !mcss_prop_is_active( *tag_prop ) &&
+            !mcss_prop_is_active( *effect_prop )
+         ) || (
+            /* No competition. */
+            NULL == tag_prop &&
+            !mcss_prop_is_active( *effect_prop )
+         )
+      ))
+   ) {
+      /* Inherit parent property. */
+      if( MCSS_PROP_BACKGROUND_COLOR == p_id ) {
+#if MCSSMERG_TRACE_LVL > 0
+         debug_printf( MCSSMERG_TRACE_LVL, "background color was %s",
+            0 <= *(RETROFLAT_COLOR*)effect_prop ?
+            gc_retroflat_color_names[*(RETROFLAT_COLOR*)effect_prop] : "NULL" );
+#endif /* MCSSMERG_TRACE_LVL */
+      } else if( MCSS_PROP_COLOR == p_id ) {
+#if MCSSMERG_TRACE_LVL > 0
+         debug_printf( MCSSMERG_TRACE_LVL, "color was %s",
+            0 <= *(RETROFLAT_COLOR*)effect_prop ?
+            gc_retroflat_color_names[*(RETROFLAT_COLOR*)effect_prop] : "NULL" );
+#endif /* MCSSMERG_TRACE_LVL */
+      }
+#if MCSSMERG_TRACE_LVL > 0
+      debug_printf( MCSSMERG_TRACE_LVL,
+         "%s using parent %s: " SSIZE_T_FMT,
+         gc_mhtml_tag_names[tag_type], prop_name, *(ssize_t*)parent_prop );
+#endif /* MCSSMERG_TRACE_LVL */
+      /* XXX: Assign */
+      memcpy( effect_prop, parent_prop, prop_sz );
+      *effect_prop_flags = *parent_prop_flags;
+      if( MCSS_PROP_BACKGROUND_COLOR == p_id ) {
+#if MCSSMERG_TRACE_LVL > 0
+         debug_printf( MCSSMERG_TRACE_LVL, "background color %s",
+            0 <= *(RETROFLAT_COLOR*)effect_prop ?
+            gc_retroflat_color_names[*(RETROFLAT_COLOR*)effect_prop] : "NULL" );
+#endif /* MCSSMERG_TRACE_LVL */
+      } else if( MCSS_PROP_COLOR == p_id ) {
+#if MCSSMERG_TRACE_LVL > 0
+         debug_printf( MCSSMERG_TRACE_LVL, "color %s",
+            0 <= *(RETROFLAT_COLOR*)effect_prop ?
+            gc_retroflat_color_names[*(RETROFLAT_COLOR*)effect_prop] : "NULL" );
+#endif /* MCSSMERG_TRACE_LVL */
+      }
+   } else if(
+      NULL != tag_prop &&
+      mcss_prop_is_active( *tag_prop )
+   ) {
+      /* Use new property. */
+#if MCSSMERG_TRACE_LVL > 0
+      debug_printf( MCSSMERG_TRACE_LVL, "%s using style %s: " SSIZE_T_FMT,
+         gc_mhtml_tag_names[tag_type], prop_name,
+         *(ssize_t*)tag_prop );
+      if( MCSS_PROP_COLOR == p_id ) {
+         debug_printf( MCSSMERG_TRACE_LVL, "color %s",
+            0 <= *(RETROFLAT_COLOR*)effect_prop ?
+            gc_retroflat_color_names[*(RETROFLAT_COLOR*)effect_prop] : "NULL" );
+      }
+#endif /* MCSSMERG_TRACE_LVL */
+      memcpy( effect_prop, tag_prop, prop_sz );
+      *effect_prop_flags = *tag_prop_flags;
+   }
+}
+
+/* === */
+
 void mcssmerge_styles(
    struct MCSS_STYLE* effect_style,
    struct MCSS_STYLE* parent_style,
@@ -38,72 +129,16 @@ void mcssmerge_styles(
    /* Perform inheritence of special cases. */
 
    #define MCSS_PROP_TABLE_MERGE( p_id, prop_n, prop_t, prop_p, def ) \
-      if( \
-         ( \
-            /* Only do inheritence for some special cases.
-             * e.g. We don't want to inherit width/height/X/etc! */ \
-            mcss_prop_is_heritable( p_id ) \
-         ) && (NULL != parent_style && ( \
-            ( \
-               NULL != tag_style && \
-               /* Parent is important and new property isn't. */ \
-               mcss_prop_is_active_flag( parent_style->prop_n, IMPORTANT ) && \
-               /* TODO: Is not active OR important? */ \
-               !mcss_prop_is_important( tag_style->prop_n ) && \
-               !mcss_prop_is_important( effect_style->prop_n ) \
-            ) || ( \
-               NULL != tag_style && \
-               /* New property is not active. */ \
-               !mcss_prop_is_active( tag_style->prop_n ) && \
-               !mcss_prop_is_active( effect_style->prop_n ) \
-            ) || (\
-               /* No competition. */ \
-               NULL == tag_style && \
-               !mcss_prop_is_active( effect_style->prop_n ) \
-            ) \
-         )) \
-      ) { \
-         /* Inherit parent property. */ \
-         if( MCSS_PROP_BACKGROUND_COLOR == p_id ) { \
-            debug_printf( MCSSMERG_TRACE_LVL, "background color was %s", \
-               0 <= effect_style->prop_n ? \
-               gc_retroflat_color_names[effect_style->prop_n] : "NULL" ); \
-         } else if( MCSS_PROP_COLOR == p_id ) { \
-            debug_printf( MCSSMERG_TRACE_LVL, "color was %s", \
-               0 <= effect_style->prop_n ? \
-               gc_retroflat_color_names[effect_style->prop_n] : "NULL" ); \
-         } \
-         debug_printf( MCSSMERG_TRACE_LVL, \
-            "%s using parent %s: " SSIZE_T_FMT, \
-            gc_mhtml_tag_names[tag_type], #prop_n, \
-            (ssize_t)(parent_style->prop_n) ); \
-         effect_style->prop_n = parent_style->prop_n; \
-         effect_style->prop_n ## _flags = parent_style->prop_n ## _flags; \
-         if( MCSS_PROP_BACKGROUND_COLOR == p_id ) { \
-            debug_printf( MCSSMERG_TRACE_LVL, "background color %s", \
-               0 <= effect_style->prop_n ? \
-               gc_retroflat_color_names[effect_style->prop_n] : "NULL" ); \
-         } else if( MCSS_PROP_COLOR == p_id ) { \
-            debug_printf( MCSSMERG_TRACE_LVL, "color %s", \
-               0 <= effect_style->prop_n ? \
-               gc_retroflat_color_names[effect_style->prop_n] : "NULL" ); \
-         } \
-      } else if( \
-         NULL != tag_style && \
-         mcss_prop_is_active( tag_style->prop_n ) \
-      ) { \
-         /* Use new property. */ \
-         debug_printf( MCSSMERG_TRACE_LVL, "%s using style %s: " SSIZE_T_FMT, \
-            gc_mhtml_tag_names[tag_type], #prop_n, \
-            (ssize_t)(tag_style->prop_n) ); \
-         if( MCSS_PROP_COLOR == p_id ) { \
-            debug_printf( MCSSMERG_TRACE_LVL, "color %s", \
-               0 <= effect_style->prop_n ? \
-               gc_retroflat_color_names[effect_style->prop_n] : "NULL" ); \
-         } \
-         effect_style->prop_n = tag_style->prop_n; \
-         effect_style->prop_n ## _flags = tag_style->prop_n ## _flags; \
-      }
+      mcssmerge_merge_prop( p_id, \
+         #prop_n, \
+         sizeof( prop_t ), \
+         tag_type, \
+         NULL != parent_style ? &(parent_style->prop_n) : NULL, \
+         NULL != parent_style ? &(parent_style->prop_n ## _flags) : 0, \
+         NULL != effect_style ? &(effect_style->prop_n) : NULL, \
+         NULL != effect_style ? &(effect_style->prop_n ## _flags) : 0, \
+         NULL != tag_style ? &(tag_style->prop_n) : NULL, \
+         NULL != tag_style ? &(tag_style->prop_n ## _flags) : 0 );
 
    MCSS_PROP_TABLE( MCSS_PROP_TABLE_MERGE )
 
