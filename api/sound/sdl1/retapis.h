d258d82609b85a8fcd24024f3966989e3ca7597f
indigoparadox <indigo.repo@interfinitydynamics.info>
2026-04-19 10:36:48 -0400

    Start reorganizing sound API for new thick synth.

--- api/sound/sdl1/retapis.h
+++ api/sound/sdl1/retapis.h
@@ -63,20 +63,7 @@ cleanup:
 
 /* === */
 
-void retrosnd_midi_set_sf_bank( const char* filename_in ) {
-   if(
-      RETROSND_FLAG_INIT !=
-      (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
-   ) {
-      return;
-   }
-
-#     pragma message( "warning: set_sf_bank not implemented" )
-}
-
-/* === */
-
-void retrosnd_midi_set_voice( uint8_t channel, uint8_t voice ) {
+void retrosnd_set_voice( uint8_t channel, uint8_t voice ) {
    if(
       RETROSND_FLAG_INIT !=
       (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
@@ -89,7 +76,7 @@ void retrosnd_midi_set_voice( uint8_t channel, uint8_t voice ) {
 
 /* === */
 
-void retrosnd_midi_set_control( uint8_t channel, uint8_t key, uint8_t val ) {
+void retrosnd_set_control( uint8_t channel, uint8_t key, uint8_t val ) {
    if(
       RETROSND_FLAG_INIT !=
       (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
@@ -102,7 +89,7 @@ void retrosnd_midi_set_control( uint8_t channel, uint8_t key, uint8_t val ) {
 
 /* === */
 
-void retrosnd_midi_note_on( uint8_t channel, uint8_t pitch, uint8_t vel ) {
+void retrosnd_note_on( uint8_t channel, uint8_t pitch, uint8_t vel ) {
    if(
       RETROSND_FLAG_INIT !=
       (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
@@ -115,7 +102,7 @@ void retrosnd_midi_note_on( uint8_t channel, uint8_t pitch, uint8_t vel ) {
 
 /* === */
 
-void retrosnd_midi_note_off( uint8_t channel, uint8_t pitch, uint8_t vel ) {
+void retrosnd_note_off( uint8_t channel, uint8_t pitch, uint8_t vel ) {
    if(
       RETROSND_FLAG_INIT !=
       (RETROSND_FLAG_INIT & g_retroflat_state->sound.flags)
@@ -124,51 +111,6 @@ void retrosnd_midi_note_off( uint8_t channel, uint8_t pitch, uint8_t vel ) {
    }
 
 #     pragma message( "warning: note_off not implemented" )
-}
-
-/* === */
-
-MERROR_RETVAL retrosnd_midi_play_smf( const char* filename ) {
-   MERROR_RETVAL retval = MERROR_OK;
-
-   /* TODO: Use chunks and play at pitch/velocity? */
-
-   if( Mix_PlayingMusic() ) {
-      debug_printf( RETROSND_TRACE_LVL, "stopping music..." );
-      Mix_HaltMusic();
-   }
-
-   if( NULL != g_retroflat_state->sound.music ) {
-      debug_printf( RETROSND_TRACE_LVL, "unloading music..." );
-      Mix_FreeMusic( g_retroflat_state->sound.music );
-      g_retroflat_state->sound.music = NULL;
-   }
-
-   if( NULL == filename ) {
-      goto cleanup;
-   }
-
-   /* Attempt to load requested music. */
-
-   g_retroflat_state->sound.music = Mix_LoadMUS( filename );
-   if( NULL == g_retroflat_state->sound.music ) {
-      error_printf( "could not load \"%s\": %s", filename, Mix_GetError() );
-      retval = MERROR_SND;
-   }
-
-   debug_printf( RETROSND_TRACE_LVL, "%s loaded, playing...", filename );
-
-   Mix_PlayMusic( g_retroflat_state->sound.music, -1 );
-
-cleanup:
-
-   return retval;
-}
-
-/* === */
-
-uint8_t retrosnd_midi_is_playing_smf() {
-   return Mix_PlayingMusic();
 }
 
 /* === */
--- src/retrosnd.h
+++ src/retrosnd.h
@@ -1,5 +1,103 @@
 
 #ifndef RETROSND_H
 #define RETROSND_H
+
+/**
+ * \addtogroup maug_retrosnd RetroSound API
+ * \brief Abstraction layer header for sound on retro systems.
+ * \{
+ *
+ * \file retrosnd.h
+ * \brief Abstraction layer header for sound on retro systems.
+ *
+ * RetroSound is a compatibility layer for making sound
+ * on various platforms, including Windows, MS-DOS or Linux.
+ *
+ * To use, define RETROSND_C before including this header from your main.c.
+ *
+ * You may include this header in other .c files, as well, but RETROSND_C
+ * should \b ONLY be declared \b ONCE in the entire program.
+ *
+ * It is *highly* advised to use this in conjunction with retroflt.h.
+ *
+ * maug.h should also be included before this header.
+ *
+ * Special thanks to: "Programming the Adlib/Sound Blaster FM Music Chips"
+ * by Jeffrey S. Lee (https://bochs.sourceforge.io/techspec/adlib_sb.txt)
+ * and "The Gravis Ultrasound" by neuraldk
+ * (http://neuraldk.org/document.php?gus) for DOS platform-specific stuff.
+ *
+ * And the MPU-401 interface for being so darn simple!
+ */
+
+#ifndef RETROSND_TRACE_LVL
+#  define RETROSND_TRACE_LVL 0
+#endif /* !RETROSND_TRACE_LVL */
+
+#ifndef RETROSND_REG_TRACE_LVL
+#  define RETROSND_REG_TRACE_LVL 0
+#endif /* !RETROSND_REG_TRACE_LVL */
+
+/**
+ * \addtogroup maug_retrosnd_flags RetroSound State Flags
+ * \brief Flags indicating global state for the RETROSND_STATE::flags field.
+ * \{
+ */
+
+/**
+ * \brief Flag in RETROSND_STATE::flags indicating initialization was
+ *        successful.
+ */
+#define RETROSND_FLAG_INIT 0x01
+
+/*! \} */ /* maug_retrosnd_flags */
+
+#define RETROSND_VOICE_BREATH       122
+
+#define RETROSND_VOICE_SEASHORE     123
+
+#define RETROSND_VOICE_BIRD_TWEET   124
+
+#define RETROSND_VOICE_PHONE_RING   125
+
+#define RETROSND_VOICE_HELICOPTER   126
+
+#define RETROSND_VOICE_APPLAUSE     127
+
+/**
+ * \brief Parameter for retrosnd_set_voice() indicating a gunshot
+ *        sound effect.
+ */
+#define RETROSND_VOICE_GUNSHOT      128
+
+#define RETROSND_CHANNEL_CT         8
+
+/**
+ * \brief Initialize retrosnd engine.
+ * \param args A pointer to the RETROSND_ARGS struct initialized by
+ *        the calling program.
+ *
+ * The RETROSND_ARGS::snd_io_base field must be initialized with the address
+ * or other platform-specific indicator of the MIDI device to use.
+ */
+MERROR_RETVAL retrosnd_init( struct RETROFLAT_ARGS* args );
+
+/**
+ * \brief Set the name of the voice bank filename to use.
+ */
+void retrosnd_set_sf_bank( const char* filename_in );
+
+void retrosnd_set_voice( uint8_t channel, uint8_t voice );
+
+void retrosnd_set_control( uint8_t channel, uint8_t key, uint8_t val );
+
+void retrosnd_note_on( uint8_t channel, uint8_t pitch, uint8_t vel );
+
+void retrosnd_note_off( uint8_t channel, uint8_t pitch, uint8_t vel );
+
+void retrosnd_shutdown();
+
+/*! \} */ /* maug_retrosnd */
+
 #endif /* !RETROSND_H */
 
