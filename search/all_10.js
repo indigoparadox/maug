ad15e0b9872bf64eee15036509207a281ab5edec
Forgejo <forgejo@interfinitydynamics.info>
2026-04-28 04:33:16 +0000

    Committing docs.

---
+++ search/all_10.js
@@ -0,0 +1,23 @@
+var searchData=
+[
+  ['screen_5fcolors_0',['screen_colors',['../structRETROFLAT__STATE.html#a336ba0e9eb513f6d299673c4b7060a9f',1,'RETROFLAT_STATE::screen_colors()'],['../structRETROFLAT__ARGS.html#aa6a12cfc425d50d8c645f632b9a685f7',1,'RETROFLAT_ARGS::screen_colors()']]],
+  ['screen_5fh_1',['screen_h',['../structRETROFLAT__ARGS.html#a7407aaec24ac0b212ce12d92858ccc59',1,'RETROFLAT_ARGS::screen_h()'],['../structRETROFLAT__STATE.html#a1636f2ef1a0469d4887ae0df552dc9a9',1,'RETROFLAT_STATE::screen_h()']]],
+  ['screen_5fscale_2',['screen_scale',['../structRETROFLAT__STATE.html#acdb715ab3f69c0b107d89bdf72695926',1,'RETROFLAT_STATE']]],
+  ['screen_5fv_5fh_3',['screen_v_h',['../structRETROFLAT__STATE.html#ac1e7fe7127d595465bce4d9ec15bc3d1',1,'RETROFLAT_STATE']]],
+  ['screen_5fv_5fw_4',['screen_v_w',['../structRETROFLAT__STATE.html#a2fd654318e4d00f38cd68c6e8a55217b',1,'RETROFLAT_STATE']]],
+  ['screen_5fw_5',['screen_w',['../structRETROFLAT__ARGS.html#a42d9ce1abcd8638d1c4baf0a8b521bce',1,'RETROFLAT_ARGS::screen_w()'],['../structRETROFLAT__STATE.html#a5e85c4d16b4209f82a189f433d106815',1,'RETROFLAT_STATE::screen_w()']]],
+  ['screen_5fx_6',['screen_x',['../structRETROFLAT__ARGS.html#aef6028d6a8f6c54d75623f8f0f7dbc15',1,'RETROFLAT_ARGS']]],
+  ['screen_5fy_7',['screen_y',['../structRETROFLAT__ARGS.html#acba814758d8d897ecf4c692c074814c5',1,'RETROFLAT_ARGS']]],
+  ['sect_5fh_8',['sect_h',['../structRETROTILE__DATA__DS.html#adf49707aeb1660edf27a9c5ed2c8be82',1,'RETROTILE_DATA_DS']]],
+  ['sect_5fh_5fhalf_9',['sect_h_half',['../structRETROTILE__DATA__DS.html#ae0ec669d8d55925297cd8922021c2122',1,'RETROTILE_DATA_DS']]],
+  ['sect_5fw_10',['sect_w',['../structRETROTILE__DATA__DS.html#a26954d4c8767996340629414d0e38ec9',1,'RETROTILE_DATA_DS']]],
+  ['sect_5fw_5fhalf_11',['sect_w_half',['../structRETROTILE__DATA__DS.html#a454241426fe1fef7d630c9714e332a1a',1,'RETROTILE_DATA_DS']]],
+  ['sect_5fx_12',['sect_x',['../structRETROTILE__DATA__DS.html#a8220cdaa62874ad9570e90598764aaec',1,'RETROTILE_DATA_DS']]],
+  ['sect_5fy_13',['sect_y',['../structRETROTILE__DATA__DS.html#a081a6ff35940c41ac01eaa636c7aef99',1,'RETROTILE_DATA_DS']]],
+  ['serialization_2fdeserialization_20api_14',['Serialization/Deserialization API',['../group__maug__serial.html',1,'']]],
+  ['serialize_20code_20generation_20tool_15',['Serialize Code Generation Tool',['../group__maug__serial__tool.html',1,'(Global Namespace)'],['../group__maug__serial__tool.html',1,'(Global Namespace)']]],
+  ['serialize_20tool_20custom_20types_16',['Serialize Tool Custom Types',['../group__maug__serial__tool.html',1,'']]],
+  ['stack_17',['stack',['../structMLISP__EXEC__STATE.html#a2cdcdc7d0e3d80a343b6d5cee77b8c0f',1,'MLISP_EXEC_STATE']]],
+  ['static_5frotation_18',['static_rotation',['../structRETROTILE__TILE__DEF.html#a5a0e14997a4e8dbc8b659e1f7834fb8c',1,'RETROTILE_TILE_DEF']]],
+  ['sz_19',['sz',['../structRETROSND__TUNE.html#afb78f61ffbac9abd6767a34f76aa09d3',1,'RETROSND_TUNE::sz()'],['../structMFILE__CADDY.html#ae6a54cc97a71c1ee7e45c6eb97b84279',1,'MFILE_CADDY::sz()'],['../structMFMT__STRUCT.html#aae861ab44ef1fcb11785876f19305f5d',1,'MFMT_STRUCT::sz()'],['../structMFMT__STRUCT__BMPINFO.html#a7ad7a22540f5a3fbc2c92e1237eab4ee',1,'MFMT_STRUCT_BMPINFO::sz()'],['../structRETROTILE__TILE__DEF.html#aef7cc1fc6b6c2aae003fdc653e9eb1c7',1,'RETROTILE_TILE_DEF::sz()'],['../structRETROTILE__LAYER.html#a3b96c58eb7d3d0d521e630d9a5e2f0e6',1,'RETROTILE_LAYER::sz()'],['../structRETROTILE.html#aac4e928822e2761a4ad9f56b2891ab32',1,'RETROTILE::sz()']]]
+];
